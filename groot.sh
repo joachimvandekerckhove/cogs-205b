f73ba6a641c63a3a0cf0fe14a98c2171f8fa513a
doreen hii <doreen.hii@uci.edu>
2021-01-15 15:24:58 -0800

    Adding groot.sh

--- groot.sh
+++ groot.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 PSEUDONYM='groot'
-LOCAL_DIRECTORY='/Users/doreenhiiyiijiehii/Desktop/Winter2021/PSYCH205B/'
+LOCAL_DIRECTORY='/Users/doreenhiiyiijiehii/Desktop/Winter2021/PSYCH205B'
 
 # clone the class git repo
 git -C $LOCAL_DIRECTORY clone git@github.com:doreenhii/cogs-205b.git 
@@ -13,11 +13,10 @@ git -C $LOCAL_DIRECTORY/cogs-205b checkout -b upload_assignment
 mkdir $LOCAL_DIRECTORY/cogs-205b/assignment/$PSEUDONYM 
 
 # makes a copy of this file in the new directory 
-curr_sh_path=$(dirname realpath $0)
-cp curr_sh_path $LOCAL_DIRECTORY/cogs-205b/assignment/$PSEUDONYM/$0
+cp $0 $LOCAL_DIRECTORY/cogs-205b/assignment/$PSEUDONYM
 
 # push 
 git -C $LOCAL_DIRECTORY/cogs-205b add .
 git -C $LOCAL_DIRECTORY/cogs-205b commit -a -m "Adding groot.sh"
-git push origin upload_assignment
+git push --set-upstream origin upload_assignment
 
