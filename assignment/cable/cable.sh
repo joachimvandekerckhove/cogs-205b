db8c76badf47e3f20e796b440306519e0efd9a07
Brandon Hackney <bhackney@uci.edu>
2021-01-15 18:09:40 -0800

    Remove use of cd

--- assignment/cable/cable.sh
+++ assignment/cable/cable.sh
@@ -1,31 +1,37 @@
+#!/bin/bash
 # Assignment 1: Github clone and push
 
 # Define dir variables
 GITNAME=brandonhackney # NO SPACE or else it thinks = is a function
 HPCNAME=cable
-BRANCHNAME=HPCNAME-assignment1
+BRANCHNAME=$HPCNAME-assignment1
+REPONAME=cogs-205b
 
-# ensure this posts back to Joachim's main
-git remote add upstream https://github.com/joachimvandekerckhove/cogs-205b.git
+# Define upstream (even if it's not used here)
+git remote add upstream https://github.com/joachimvandekerckhove/$REPONAME.git
 
 # Clone my fork of class git repository and make a new branch
-git clone git@github.com:$GITNAME/cogs-205b.git
-cd cogs-205b #this makes the code cleaner, but is not preferable going forward.
-git branch $HPCNAME-assignment1 # create new branch
-git checkout $HPCNAME-assignment1 # switch to branch
+if [ ! -d "$REPONAME" ]; then
+  git clone git@github.com:$GITNAME/$REPONAME.git
+else
+  git --git-dir=$REPONAME/.git --work-tree=$REPONAME/ fetch --all
+fi
+git --git-dir=$REPONAME/.git --work-tree=$REPONAME/ branch $BRANCHNAME # create new branch
+git --git-dir=$REPONAME/.git --work-tree=$REPONAME/ checkout $BRANCHNAME # switch to branch
 
 # Make yourself a new directory in the class repository
-mkdir assignment/$HPCNAME
+COPYDIR=$REPONAME/assignment/$HPCNAME
+if [ ! -d "$COPYDIR" ]; then
+  mkdir $COPYDIR
+fi
 
 # Copy this script into the new directory
-cd
-cp $HPCNAME.sh cogs-205b/assignment/$HPCNAME/
+#cp $HPCNAME.sh $COPYDIR/ #lazy way
+cp "$(basename $0)" $COPYDIR/ #better way (just using $0 will give "./name" instead of "name")
 
 # Push update to git repository
-cd cogs-205b
-git add -A # Add all current changes to 'staging area'
-git commit -m "Add $HPCNAME to assignment" # Label your changes
-#git push -u upstream $HPCNAME-assignment1 # Send changes to upstream (Joachim)
-# I can't push to the upstream because I don't have login privileges to it??
-git push -u origin $HPCNAME-assignment1 # send changes to local fork
+git --git-dir=$REPONAME/.git --work-tree=$REPONAME/ add -A # Add all current changes to 'staging area'
+git --git-dir=$REPONAME/.git --work-tree=$REPONAME/ commit -m "Remove use of cd" # Label your changes
+git --git-dir=$REPONAME/.git --work-tree=$REPONAME/ push -u origin $BRANCHNAME # send changes to local fork
 # Will have to manually submit a pull request
+
