17716412db1ee2cbfbb093efdbbe122a751673d5
ManuelVU <jesus_mvu@hotmail.com>
2021-01-18 22:09:10 -0800

    assignment 1, change 1

--- assignment/deadpool/deadpool.sh
+++ assignment/deadpool/deadpool.sh
@@ -1,14 +1,13 @@
 git clone git@github.com:ManuelVU/cogs-205b.git
 cd cogs-205b
+git checkout -b branch_as1
 git remote add upstream https://github.com/joachimvandekerckhove/cogs-205b.git
-git remote -v
 psudonym=deadpool
 filename=$psudonym".sh"
-git checkout -b [assignment1]
 mkdir assignment/$psudonym
 cd -
 cp $filename cogs-205b/assignment/$psudonym
 cd cogs-205b
 git add .
-git commit -m "assignment 1"
-git push origin
+git commit -m "assignment 1, change 1"
+git push origin -u branch_as1
