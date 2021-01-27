dcd8e056a521d77fad562eebba05b2728d1a124b
ManuelVU <jesus_mvu@hotmail.com>
2021-01-26 18:12:53 -0600

    adding update function for precision

--- assignment/deadpool/+deadpool/@Norm2d/Normal2d.m
+++ assignment/deadpool/+deadpool/@Norm2d/Normal2d.m
@@ -5,4 +5,27 @@ classdef Normal2d
         CovM(2,2) double {mustBeFinite,mustBePositiveSemiDef(CovM),mustBeSimm(CovM)} = eye(2) 
     end
 
+    properties (constant)
+        scalingconstant = (2*pi).^(-0.5);
+    end
+
+    properties (SetAccess = private)
+        PrecisionM
+        Correlation
+    end
+
+    methods
+        function obj = Normal2d(Mean, CovM)
+            if nargin > 0
+                % Set mean of 2D MVN
+                obj.Mean = Mean;
+                if nargin > 1
+                    % Set Covariance matrix of 2D MVN
+                    obj.CovM = CovM;
+                end
+            end
+            obj = updatePresCorM(obj)
+        end
+    end
+
 end
