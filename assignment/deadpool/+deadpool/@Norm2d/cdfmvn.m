e4c8df212c6a72e63c0d2f058aa01db277df7354
ManuelVU <jesus_mvu@hotmail.com>
2021-01-29 17:36:06 -0600

    adding checks to cdf

--- assignment/deadpool/+deadpool/@Norm2d/cdfmvn.m
+++ assignment/deadpool/+deadpool/@Norm2d/cdfmvn.m
@@ -3,19 +3,18 @@ function Fx = cdfmvn(obj,xax)
     % xax has to be a matrix of column vectors of size 2xn
 
 % Validate properties of the point xax
+% Check xax is a real number 
+    if ~(isnumeric(xax) & isfinite(xax) & isreal(xax))
+        error('Support must be a real finite value')
+    end
 % Is xax a matrix of dimentions 2,n?        
-    if ~(size(xax,1)==2)
+    if ~(size(xax,1)==2|size(xax,2)==2)
         error('Support must have size of 2.')
     end
 
-% Are the values on xax finite?
-    if ~(isfinite(xax))
-        error('Support must be a finite value.')
-    end
-
-% Are the values on xax real numbers?
-    if ~(isreal(xax))
-        error('Support must be a real vector.')
+% Transpose vector if it is not column
+    if (size(xax,2)==2)
+        xax = xax.';
     end
 
 % Number of 2d points to evaluate
