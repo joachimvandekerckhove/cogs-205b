c348d2e58b0e2074e4117970a89806e3a1d3c6f3
doreen hii <doreen.hii@uci.edu>
2021-01-30 16:36:37 -0800

    adding pdf function

--- assignment/groot/+groot/@Norm2d/pdf.m
+++ assignment/groot/+groot/@Norm2d/pdf.m
@@ -1,9 +1,28 @@
 % Probability density function
 function yax = pdf(obj, xax)
+   
+% check shape of input parameter xax
+x_size = size(xax);
+if (size(x_size) ~= 2)
+    error('Function pdf error: Parameter xax shape illegal (should be 2xN)');
+end
 
-% variance_x = 
+% If xax is Nx2, restructure 
+if (x_size(1) ~= 2)
+    if (x_size(2) == 2)
+       xax = transpose(xax);
+    else
+        error('Function pdf error: Parameter xax shape illegal (should be 2xN)');
+    end
+end
 
-knl = exp( -0.5 * obj.Precision * (obj.Mean - xax).^2);
-yax = obj.ScalingConstant * obj.Precision * knl;
+z_element1 = (xax(1,:) - obj.Mean(1))/obj.StandardDev(1);
+z_element2 = (xax(2,:) - obj.Mean(2))/obj.StandardDev(2);
+z = (z_element1).^2 - (2*obj.Correlation .* z_element1 .* z_element2) + ...
+    (z_element2).^2;
+knl = exp(-0.5 * z / (1-obj.Correlation^2));
+scaling = 1/(2*pi*obj.StandardDev(1)*obj.StandardDev(2)*sqrt(1-obj.Correlation^2));
+
+yax = scaling * knl;
 
 end
