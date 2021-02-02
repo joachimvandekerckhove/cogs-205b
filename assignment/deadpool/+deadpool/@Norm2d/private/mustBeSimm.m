93712c409d447e6a496eee7a1fcbb191758ee689
ManuelVU <jesus_mvu@hotmail.com>
2021-02-02 17:03:48 -0600

    fixing sim function 4

--- assignment/deadpool/+deadpool/@Norm2d/private/mustBeSimm.m
+++ assignment/deadpool/+deadpool/@Norm2d/private/mustBeSimm.m
@@ -1,6 +1,6 @@
 function mustBeSimm(obj)
 % Validates if the covariance matrix is positive semi-definite
-    if ~(obj.Covariance==transpose(obj.Covariance))
+    if ~(obj==transpose(obj))
         eidType = "CovM:notsymmetric"
         msgType = 'Covariance matrix is not Simmetryc.';
         throwAsCaller(MException(eidType,msgType))
