cd3adc6ca76ac68e7603ffe269ee3f72f6995ac9
doreen hii <doreen.hii@uci.edu>
2021-02-03 21:32:44 -0800

    renamed rng to rnd, changed size of rnd output to meet expectations

--- assignment/groot/+groot/@Norm2d/rnd.m
+++ assignment/groot/+groot/@Norm2d/rnd.m
@@ -1,13 +1,13 @@
-function yax = rng(obj, size)
+function yax = rnd(obj, size)
    % RNG Random number generation for Norm2d
    % returns a 2 x size matrix 
    
-% Generate random samples from first distribution (Nx1 vector)
-x1 = random('Normal', obj.Mean(1), obj.StandardDev(1), [size,1]);
+% Generate random samples from first distribution (1xN vector)
+x1 = random('Normal', obj.Mean(1), obj.StandardDev(1), [1,size]);
 x2_mean = obj.Mean(2) + obj.StandardDev(2) * obj.Correlation * ...
     ((x1 - obj.Mean(1)) / obj.StandardDev(1));
 x2_std = obj.Covariance(2,2) * sqrt(1 - obj.Correlation^2);
-x2 = random('Normal', x2_mean, x2_std, [size,1]);
+x2 = random('Normal', x2_mean, x2_std, [1,size]);
 
-yax = [x1 x2];
+yax = [x1; x2];
 end
