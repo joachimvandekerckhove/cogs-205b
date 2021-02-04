5e0380b060d11045d77fa1d5656deec98059d172
doreen hii <doreen.hii@uci.edu>
2021-02-03 21:23:06 -0800

    corrected handling of mean shape

--- assignment/groot/+groot/@Norm2d/logcdf.m
+++ assignment/groot/+groot/@Norm2d/logcdf.m
@@ -3,7 +3,7 @@ function yax = logcdf(obj, xax)
 
 validateattributes(xax,{'numeric'}, {'ndims',2,'nrows',2});
 try
-    yax = log(mvncdf(transpose(xax), obj.Mean, obj.Covariance));
+    yax = log(mvncdf(transpose(xax), transpose(obj.Mean), obj.Covariance));
 catch e
     error(e.message);
 end
