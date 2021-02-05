3c15d4363fc7f6ece11d95cdbc7d1a92eb05a68b
doreen hii <doreen.hii@uci.edu>
2021-02-05 14:19:27 -0800

    fixed typo

--- assignment/deadpool/+deadpool/@Norm2d/private/mustBeSimm.m
+++ assignment/deadpool/+deadpool/@Norm2d/private/mustBeSimm.m
@@ -2,7 +2,7 @@ function mustBeSimm(obj)
 % Validates if the covariance matrix is positive semi-definite
     if ~(issymmetric(obj))
         eidType = "Covariance:notsymmetric";
-        msgType = 'Covariance matrix is not Simmetryc.';
+        msgType = 'Covariance matrix is not Symmetric.';
         throwAsCaller(MException(eidType,msgType));
     end
 end
