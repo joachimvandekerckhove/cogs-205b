a7331d6ef67bcfc87809351c76f4055f61a012e5
zhibin zhou <zhibinz2@uci.edu>
2021-02-04 23:52:04 -0800

    add p file

--- assignment/Adam/+Adam/@Norm2d/private/updateCovariance.m
+++ assignment/Adam/+Adam/@Norm2d/private/updateCovariance.m
@@ -1,8 +1,8 @@
 % Update Correlation and Precision based on Covariance
 function obj = updateCovariance(obj)
 % update precision
-obj.Precision=inv(obj.Coveriance);
+obj.Precision=inv(obj.Covariance);
 % update correlation (rho)
-obj.Correlation=obj.Covariance(1,2)/(sqrt(obj.Covariance(1,1))*sqrt(obj.Covariance(2,2));
+obj.Correlation=obj.Covariance(1,2)/(sqrt(obj.Covariance(1,1))*sqrt(obj.Covariance(2,2)));
 end
 
