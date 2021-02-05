7bae8f6a086622f91b2dcbd8dd048a10a21c0215
ManuelVU <jesus_mvu@hotmail.com>
2021-02-05 16:57:47 -0600

    moving deviance function to its own file

--- assignment/cable/+cable/@Norm2d/Norm2d.m
+++ assignment/cable/+cable/@Norm2d/Norm2d.m
@@ -28,15 +28,7 @@ classdef Norm2d
             obj = set.Mean(obj,mu);
             obj = set.Covar(obj,sigma);
         end
-        
-        % Math stuff
-        function dev = deviance(Data,Mu,Sigma)
-            % Data is an n*2 matrix (2 cols, n rows)
-            X = linspace(1,size(Data,1),size(Data,1));
-            X = [X,X];
-            dev = (-2) * sum(logpdf(X,Mu,Sigma),'all');
-        end
-        
+                
         % Getters
         function meanOut = get.Mean(obj)
             meanOut = obj.Mean;
