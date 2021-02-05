4c21b93bc5a9136bd0601d337cd44476f619a831
Nidhi Venkatanarayan Banavar <nbanavar@uci.edu>
2021-02-05 15:09:39 -0800

    delete redundant variable subFactor

--- assignment/groot/+groot/@Norm2d/logpdf.m
+++ assignment/groot/+groot/@Norm2d/logpdf.m
@@ -10,8 +10,8 @@ z_element1 = (xax(1,:) - obj.Mean(1))/obj.StandardDev(1);
 z_element2 = (xax(2,:) - obj.Mean(2))/obj.StandardDev(2);
 z = (z_element1).^2 - (2*obj.Correlation .* z_element1 .* z_element2) + ...
     (z_element2).^2;
-subfactor = 1-obj.Correlation^2;
-knl = -0.5 * z / subfactor;
+
+knl = -0.5 * z /(1-obj.Correlation^2);
 
 yax = obj.Scaling + knl;
 
