822502ce027e43914d39fb752090e9119ace9684
ManuelVU <jesus_mvu@hotmail.com>
2021-02-05 16:56:27 -0600

    moving rng function to its own file and renaming as rnd for testSuite

--- assignment/cable/+cable/@Norm2d/Norm2d.m
+++ assignment/cable/+cable/@Norm2d/Norm2d.m
@@ -30,22 +30,6 @@ classdef Norm2d
         end
         
         % Math stuff
-
-        function sample = rng(Mu,Sigma,dimMat)
-            % Pull a random sample from this distribution
-            verifyMean(Mu);
-            verifySigma(Sigma);
-            verifySize(dimMat);
-            % Preallocate
-            sample = zeros(dimMat);
-            fprintf(1,"This function isn't ready yet, sorry.\n")
-            % Sample from first distribution
-%             x1 = Mu(1) .* rand(size(sample(:,1)));
-%             sample(:,1) = pdf(x1,Mu(1),Sigma(1));
-%             % sample from second distribution
-%             sample(:,2) = pdf(; % use sample(:,1), mu(2), and sigma(4)
-
-        end
         function dev = deviance(Data,Mu,Sigma)
             % Data is an n*2 matrix (2 cols, n rows)
             X = linspace(1,size(Data,1),size(Data,1));
