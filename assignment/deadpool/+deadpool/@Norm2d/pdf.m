aa5865f98a613d083f7dc7586b5b986477d62348
doreen hii <doreen.hii@uci.edu>
2021-02-05 15:45:21 -0800

    revert vectorization, not necessarily more efficient

--- assignment/deadpool/+deadpool/@Norm2d/pdf.m
+++ assignment/deadpool/+deadpool/@Norm2d/pdf.m
@@ -6,11 +6,19 @@ check_xax(xax)
 
 % Number of 2d points to evaluate
     npoints = size(xax,2);
-
-% xax is 2xN, reformat obj.Mean to the same size
-    krnl = exp(-0.5*((xax - repmat(obj.Mean, [1, npoints])).' * ...
-        obj.Precision * (xax - repmat(obj.Mean, [1, npoints]))));
-    dF = obj.scalingConstant * obj.invsqrtdet * krnl; % output is NxN (need only the diag for Nx1)
-    dF = diag(dF).';
+    
+% Create a vector to save multiple values of the kernel for the MVN
+    krnl = zeros(npoints,1);
+ 
+% Create a vector to save multiple values of the density of the MVN
+    dF = zeros(npoints,1);
+ 
+    for i = 1:npoints
+% Evaluate the kernel at point xax
+        krnl(i) = exp(-0.5*((xax(:,i) - obj.Mean).' * obj.Precision * (xax(:,i) - obj.Mean)));
+% Evaluate density function at point xax
+        dF(i) = obj.scalingConstant * obj.invsqrtdet * krnl(i);
+    end
+    dF = dF.' ;
 
 end
