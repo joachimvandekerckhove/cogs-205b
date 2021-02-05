5f50ebec9f37ea63f0ee8832156a3c33b7790f97
ManuelVU <jesus_mvu@hotmail.com>
2021-02-05 17:10:21 -0600

    moving checkReal function to its own file in private

--- assignment/cable/+cable/@Norm2d/Norm2d.m
+++ assignment/cable/+cable/@Norm2d/Norm2d.m
@@ -151,15 +151,6 @@ classdef Norm2d
         end
         
         % Subfunctions
-
-        function checkReal(var,varStr)
-            % Checks whether any matrix elements are imaginary
-            % varStr is a string of the variable's name, for errors
-            A = find(~isreal(var));
-            if A
-                error("Error: Element %i of %s is imaginary.",A(1),varStr);
-            end
-        end
         function checkNumber(var,varStr)
             if ~isnumeric(var)
                 error("Error: %s is not a numeric variable.",varStr);
