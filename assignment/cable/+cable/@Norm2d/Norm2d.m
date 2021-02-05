34b90b56e6ea70b777b83898c2ef62bfb944e872
ManuelVU <jesus_mvu@hotmail.com>
2021-02-05 17:08:44 -0600

    create private directory with check functions add checkInf to directory

--- assignment/cable/+cable/@Norm2d/Norm2d.m
+++ assignment/cable/+cable/@Norm2d/Norm2d.m
@@ -28,7 +28,7 @@ classdef Norm2d
             obj = set.Mean(obj,mu);
             obj = set.Covar(obj,sigma);
         end
-                
+
         % Getters
         function meanOut = get.Mean(obj)
             meanOut = obj.Mean;
@@ -151,14 +151,7 @@ classdef Norm2d
         end
         
         % Subfunctions
-        function checkInf(var,varStr)
-            % Checks whether any matrix elements are infinity
-            % varStr is a string of the variable's name, for errors
-            A = find(var == Inf);
-            if A
-                error("Error: Element %i of %s must be finite.",A(1),varStr);
-            end
-        end
+
         function checkReal(var,varStr)
             % Checks whether any matrix elements are imaginary
             % varStr is a string of the variable's name, for errors
