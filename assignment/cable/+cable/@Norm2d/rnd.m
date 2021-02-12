0f7b8429108e9f916ee9e20f7c78cac82e2e35dc
Brandon Hackney <bhackney@uci.edu>
2021-02-12 12:47:45 -0800

    Finish rnd.m

--- assignment/cable/+cable/@Norm2d/rnd.m
+++ assignment/cable/+cable/@Norm2d/rnd.m
@@ -1,14 +1,15 @@
-function sample = rnd(Mu,Sigma,dimMat)
+function sample = rnd(obj,dimMat)
     % Pull a random sample from this distribution
-    verifyMean(Mu);
-    verifySigma(Sigma);
+    % dimMat should have two dimensions
     verifySize(dimMat);
-    % Preallocate
-    sample = zeros(dimMat);
-    fprintf(1,"This function isn't ready yet, sorry.\n")
+%     fprintf(1,"This function isn't ready yet, sorry.\n")
     % Sample from first distribution
-%             x1 = Mu(1) .* rand(size(sample(:,1)));
-%             sample(:,1) = pdf(x1,Mu(1),Sigma(1));
-%             % sample from second distribution
-%             sample(:,2) = pdf(; % use sample(:,1), mu(2), and sigma(4)
+%         x1 = obj.Mean(1) .* rand([1,dimMat(1)]);
+%         x2 = obj.Mean(2) .* rand([1,dimMat(2)]);
+        x1 = normrnd(obj.Mean(1),obj.Covariance(1),dimMat);
+    % Sample from second distribution
+        sample = normrnd((obj.Mean(2) + ...
+            (obj.Rho * sqrt(obj.Covariance(4))) .* ...
+            (x1 - obj.Mean(1))./(sqrt(obj.Covariance(1)))),...
+            (obj.Covariance(4) .* sqrt(1 - obj.Rho ^2)),dimMat);
 end
