8dcf790f88c7a47af65587750eabf5314c9082a6
ManuelVU <jesus_mvu@hotmail.com>
2021-02-15 17:02:46 -0600

    adding estimate function to validate properties of data matrix

---
+++ assignment/deadpool/+deadpool/@Norm2d/estimate.m
@@ -0,0 +1,5 @@
+function estimate(X)
+% This function takes a 2xN matrix and returns an object of the class
+% Norm2d with mean vector equal to the mean of the rows and a Covariance matrix.
+    Check_data(X);
+end
