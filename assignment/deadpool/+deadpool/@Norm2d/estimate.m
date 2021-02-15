e7a499e459f1965e6f0dbb96ea82a203b2a0ca17
ManuelVU <jesus_mvu@hotmail.com>
2021-02-15 17:28:57 -0600

    fix mean estimate

--- assignment/deadpool/+deadpool/@Norm2d/estimate.m
+++ assignment/deadpool/+deadpool/@Norm2d/estimate.m
@@ -1,4 +1,4 @@
-function estimate(obj,X)
+function estimate(X)
 % This function takes a 2xN matrix and returns an object of the class
 % Norm2d with mean vector equal to the mean of the rows and a Covariance matrix.
     Check_data(X);
