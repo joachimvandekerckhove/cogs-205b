0a07f0da0215cbdbe27d7659191a3dae9dc1c05c
ManuelVU <jesus_mvu@hotmail.com>
2021-02-15 17:08:06 -0600

    fix check data

--- assignment/deadpool/+deadpool/@Norm2d/private/Check_data.m
+++ assignment/deadpool/+deadpool/@Norm2d/private/Check_data.m
@@ -3,21 +3,21 @@ function Check_data(datos)
 
     % Are datos a matrix of dimentions 2xN?
     if ~(size(datos,1)==2)
-        error('Data has to be a matrix of size 2 x N.')
+        error('Data has to be a matrix of size 2 x N.');
     end
 
     % Are datos numerical values?
     if ~(isnumeric(datos))
-        error('Data must contain only numeric values.')
+        error('Data must contain only numeric values.');
     end
 
     % Are datos real numbers?  
     if ~(isreal(datos))
-        error("Data must contain only real numbers.")
+        error('Data must contain only real numbers.')
     end
 
     % Are datos finite?
     if ~(isfinite(datos))
-        error("Data must contain only finite values.")
+        error('Data must contain only finite values.')
     end
 end
