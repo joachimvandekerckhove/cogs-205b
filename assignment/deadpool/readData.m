be0295942ebbf78961d0d0de36b98815f05fc19c
ManuelVU <jesus_mvu@hotmail.com>
2021-02-16 17:48:01 -0600

    stop output from readdata

--- assignment/deadpool/readData.m
+++ assignment/deadpool/readData.m
@@ -2,21 +2,21 @@ function data = readData(path)
 % This function reads a data file saved on a csv file where the names 
 % of the variables are stored on the first row
     if ~exist(path,'file')
-        error('Directory: %s \n does not exist', path)
+        error('Directory: %s \n does not exist', path);
     end
 
-    tmp = fopen(path) % open the file to object tmp
-    names = fgetl(tmp)       % read the first line
-    fclose(tmp)
+    tmp = fopen(path); % open the file to object tmp
+    names = fgetl(tmp);       % read the first line
+    fclose(tmp);
 
     if ~(isnumeric(names))
         % Read data file in path with first row as names
-        tmp_dat = readtable(path,'HeaderLines', 1)
+        tmp_dat = readtable(path,'HeaderLines', 1);
 
         % Transform table to a matrix and save it as data
-        data = tmp_dat{:,:}
+        data = tmp_dat{:,:};
     else 
-        tmp_dat = readtable(path)
-        data = tmp_dat{:,:}
+        tmp_dat = readtable(path);
+        data = tmp_dat{:,:};
     end
 end
