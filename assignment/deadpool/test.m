c64f407683be54856f0107310ade9c6dc80be665
ManuelVU <jesus_mvu@hotmail.com>
2021-02-15 20:35:45 -0600

    test for matrix size

--- assignment/deadpool/test.m
+++ assignment/deadpool/test.m
@@ -16,45 +16,57 @@ function test(pseudonym)
     throwAsCaller = @(x)warning(x.message);
 
 %% Report
-% Start printing a little report to screen
-% make a long line
+    % Start printing a little report to screen
+    % make a long line
     dashline = repmat('-', 1, 77);
 
-% print a line
+    % print a line
     fprintf('#%s#\n', dashline);
 
-% print time and date
+    % print time and date
     fprintf('# %74s  #\n', datestr(now))
 
-% print report title
+    % print report title
     fprintf('#%-77s#\n', sprintf('  Test suite for "%s"  ', class(this)))
 
-% print a line
+    % print a line
     fprintf('#%s#\n', dashline); 
 
-% Test for estimate function 
-% Data matrix for test
+% Test for Equality between Matlab functions and estimate.
+    % Data matrix for test
     test.data = [-1,0,1;3.2,-2,-1.2];
-% Reference: Mean vector using matlab function
+    % Reference: Mean vector using matlab function
     ref.mean = mean(test.data,2);
-% Reference: Covariance matrix using matlab function
+    % Reference: Covariance matrix using matlab function
     ref.covariance = cov(test.data.');
-% Reference: Precision matrix using matlab function
+    % Reference: Precision matrix using matlab function
     ref.pres = inv(ref.covariance);
-% Reference: Correlation from matlab function
+    % Reference: Correlation from matlab function
     ref.cor = corr(test.data.');
     ref.cor = ref.cor(1,2);
-% New object with updated properties to be tested
+    % New object with updated properties to be tested
     tobe_tested = this().estimate(test.data);
 
-% Test that the mean is correct
-    assertEqualUpToTol(ref.mean, tobe_tested.Mean, 'equallity of means')
-% Test that the Covariance matrix is correct
-    assertEqualUpToTol(ref.covariance, tobe_tested.Covariance, 'equallity of covariance matrix')
-% Test that the Presicion matrix is correct    
+    % Test that the mean is correct
+    assertEqualUpToTol(ref.mean, tobe_tested.Mean, 'equallity of Means')
+    % Test that the Covariance matrix is correct
+    assertEqualUpToTol(ref.covariance, tobe_tested.Covariance, 'equallity of Covariance matrix')
+    % Test that the Presicion matrix is correct    
     assertEqualUpToTol(ref.pres, tobe_tested.Precision, 'equallity of Presicion matrix')
-% Test that the correlation is correct    
-    assertEqualUpToTol(ref.cor, tobe_tested.Correlation, 'equality of correlation')
+    % Test that the correlation is correct    
+    assertEqualUpToTol(ref.cor, tobe_tested.Correlation, 'equality of Correlation')
+
+
+% Negative tests confirm that errors are thrown when expected
+    % Test for errors when data matrix contains values that are not allowed
+    try
+        test.data = [1:3;1:3;1:3] % wrong dimentions
+        this().estimate();  
+        errorThrown = false;
+    catch
+    errorThrown = true;
+    end
+    assertErrorThrown(errorThrown, 'Data file . enforce matrix size (2 x N)')
 
 % Wrapup
     fprintf('#%s#\n', dashline);
@@ -62,6 +74,12 @@ function test(pseudonym)
     fprintf('#%-77s#\n', sprintf('  All tests passed for "%s"  ', class(this)))
     fprintf('#%s#\n', dashline);
 
+
+
+
+
+
+% Additional functions for tests:    
 % Assert an approximate equality
     function assertEqualUpToTol(a, b, condition)
         if all(abs(a-b) < 1e-10)
@@ -72,6 +90,18 @@ function test(pseudonym)
             disp('!  I expected a==b:')
             a
             b
+            throwAsCaller(failure(condition))
+        end
+    end
+
+% Assert that an error was thrown on a call
+    function assertErrorThrown(a,condition)
+        if a
+            success(condition)
+        else
+            fprintf('#%s#\n', dashline);
+            fprintf('!  Test failed: %s\n', condition)
+            disp('!  I expected an error to be thrown')
             throwAsCaller(failure(condition))
         end
     end
