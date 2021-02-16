72887410b2b803f90c8aed06f875a35cf65b0d01
doreen hii <doreen.hii@uci.edu>
2021-02-16 12:30:13 -0800

    readData.m for 2D data

--- assignment/groot/+groot/@Norm2d/readData.m
+++ assignment/groot/+groot/@Norm2d/readData.m
@@ -1,9 +1,11 @@
 function outvar = readData(obj, infile)
 %READDATA Read data in file into variables
-% Input : infile - file containing the input (assumes 1 col file)
+% Input : infile - file containing the input
+%                  assumes each col corresponds to 1d data
 %       : num_cols - number of cols expected
 % Output: outvar - data in file in a column 
 
-    outvar = readvars(infile);
+    [outvar1, outvar2] = readvars(infile);
+    outvar = [outvar1, outvar2];
 end
 
