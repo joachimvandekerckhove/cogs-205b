a94b5ffc328f4dfd83f4d7a98ad512c3af30aa99
doreen hii <doreen.hii@uci.edu>
2021-02-16 12:25:46 -0800

    add readData.m

---
+++ assignment/groot/+groot/@Norm2d/readData.m
@@ -0,0 +1,9 @@
+function outvar = readData(obj, infile)
+%READDATA Read data in file into variables
+% Input : infile - file containing the input (assumes 1 col file)
+%       : num_cols - number of cols expected
+% Output: outvar - data in file in a column 
+
+    outvar = readvars(infile);
+end
+
