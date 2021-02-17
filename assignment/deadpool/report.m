eb5cbd311cb00cc8c5c31b61d83a23244d06c481
ManuelVU <jesus_mvu@hotmail.com>
2021-02-16 18:38:16 -0600

    first lines of report fix 2

--- assignment/deadpool/report.m
+++ assignment/deadpool/report.m
@@ -1,16 +1,13 @@
 function report(datos)
 % This function takes a matrix of size N x 2 
-    % Check Properties of the data matrix
-    Check_data(datos);   
-
     % Estimate Mean and Covariance of the variables using deadpool.Norm2d().estimate
-    result = deadpool.Norm2d().estimate(datos)
+    result = deadpool.Norm2d().estimate(datos);
 
     % Create md File with results
-    fid = fopen('deadpool-1.md','w')
-        fprintf(fid, '# Report: Mean and Covariance matrix of the vibariate variable in dataFile')
+    fid = fopen('deadpool-1.md','w');
+        fprintf(fid, '# Report: Mean and Covariance matrix of the vibariate variable in dataFile');
         fprintf(fid, '')
-        fprintf(fid, 'Date: # %74s  #\n', datestr(now))
+        fprintf(fid, 'Date: # %74s  #\n', datestr(now));
     fclose(fid)
 
 end
