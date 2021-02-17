334ab7a6e3abd876bfb452b04ce610f61613df1f
ManuelVU <jesus_mvu@hotmail.com>
2021-02-16 20:25:31 -0600

    add getData function

---
+++ assignment/deadpool/getData.m
@@ -0,0 +1,18 @@
+function path_file = getData(url)
+% This function takes a URL to a data file and downloads it, 
+% the output is the local path to the file.
+    % Check if URL is valid
+    [str,status] = urlread(url);
+    if status == 0
+         error('URL is not valid.');
+    end
+
+    % Read formated data using url
+    datos = webread(url);
+    
+    % Write table in csv file in current directory
+    writetable(datos,'dataFile.csv');
+        
+    % get path to the new data file
+    path_file = append(fileparts(which('dataFile.csv')),'/dataFile.csv');
+end
