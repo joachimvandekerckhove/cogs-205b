a57a3de125114966d064a0ae215d6e926a4581b6
ManuelVU <jesus_mvu@hotmail.com>
2021-02-16 19:53:36 -0600

    remove printing data

--- assignment/deadpool/report.m
+++ assignment/deadpool/report.m
@@ -10,16 +10,11 @@ function report(datos)
     fid = fopen('deadpool-1.md','w');
         fprintf(fid, '# Report: Mean and Covariance matrix of the input data usin class Norm2d \n');
         fprintf(fid, '\n');
-        fprintf(fid, 'Date: %74s  #\n', datestr(now));
+        fprintf(fid, 'Date: %74s  \n', datestr(now));
         fprintf(fid, '\n');
         fprintf(fid, 'This report was generated using the data matrix (size 2 x N) from the input. The mean vector and ');
         fprintf(fid, 'covariance matrix are obtained from the functions **estimate_mean** and ');
         fprintf(fid, '**estimate_covariance** in the **Norm2d** class respectively.');
-        fprintf(fid, '\n');
-        fprintf(fid, '### The First 5 rows of the data input are:');
-        fprintf(fid, '\n');
-        fprintf(fid,'%6.2f %12.8f\r\n  ', data_print);
-        fprintf(fid, '\n');
         fprintf(fid, '\n');
         fprintf(fid, '### The estimated quantities are:');
         fprintf(fid, '\n');
