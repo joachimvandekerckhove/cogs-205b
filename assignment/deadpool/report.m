92ecde4b1f2646e34b80eb611dff5e053f63d50a
ManuelVU <jesus_mvu@hotmail.com>
2021-02-16 18:42:27 -0600

    fix to report

--- assignment/deadpool/report.m
+++ assignment/deadpool/report.m
@@ -5,9 +5,9 @@ function report(datos)
 
     % Create md File with results
     fid = fopen('deadpool-1.md','w');
-        fprintf(fid, '# Report: Mean and Covariance matrix of the vibariate variable in dataFile');
-        fprintf(fid, '')
+        fprintf(fid, '# Report: Mean and Covariance matrix of the vibariate variable in dataFile \n');
+        fprintf(fid, '\n')
         fprintf(fid, 'Date: # %74s  #\n', datestr(now));
-    fclose(fid)
+    fclose(fid);
 
 end
