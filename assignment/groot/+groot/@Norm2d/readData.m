0818299411032c75d629a3f23f42c40ea4c4a6a4
doreen hii <doreen.hii@uci.edu>
2021-02-16 16:40:45 -0800

    readData.m output matrix

--- assignment/groot/+groot/@Norm2d/readData.m
+++ assignment/groot/+groot/@Norm2d/readData.m
@@ -1,9 +1,10 @@
-function [outvar1, outvar2] = readData(obj, infile)
+function outMat = readData(obj, infile)
 %READDATA Read data in file into variables
 % Input : infile - file containing the input
 %                  assumes each col corresponds to 1d data
-% Output: [outvar1, outvar2] - two columns of file data
+% Output: outMat - 2xN matrix of file data 
 
     [outvar1, outvar2] = readvars(infile);
+    outMat = [outvar1'; outvar2'];
 end
 
