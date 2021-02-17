8106056319405b5b44b23c171b3e9c6cf5f08a7e
doreen hii <doreen.hii@uci.edu>
2021-02-16 16:41:50 -0800

    accept refactor, include assertion for good objects

--- assignment/groot/+groot/@Norm2d/private/updateCovariance.m
+++ assignment/groot/+groot/@Norm2d/private/updateCovariance.m
@@ -12,5 +12,6 @@ function obj = updateCovariance(obj)
     obj.Correlation = obj.Covariance(1,2) / prod(obj.StandardDev);
     obj.Scaling = 1/(2*pi*obj.StandardDev(1)*obj.StandardDev(2)*sqrt(1-obj.Correlation^2));
    
-    assert ((-1 <= obj.Correlation) && (1 >= obj.Correlation));
+    assert ((-1 <= obj.Correlation) && (1 >= obj.Correlation), 'Bad Covariance: Correlation out of bounds');
+    assert (~isinf(obj.Scaling), 'Bad Covariance: Precision inexpressible');
 end
