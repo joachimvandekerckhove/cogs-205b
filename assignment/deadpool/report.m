8b1d30d3f5a7feda09a640cb0fce803b86248a99
ManuelVU <jesus_mvu@hotmail.com>
2021-02-16 18:56:26 -0600

    adding text to report with latex code

--- assignment/deadpool/report.m
+++ assignment/deadpool/report.m
@@ -5,9 +5,13 @@ function report(datos)
 
     % Create md File with results
     fid = fopen('deadpool-1.md','w');
-        fprintf(fid, '# Report: Mean and Covariance matrix of the vibariate variable in dataFile \n');
+        fprintf(fid, '# Report: Mean and Covariance matrix of the input data usin class Norm2d \n');
         fprintf(fid, '\n')
-        fprintf(fid, 'Date: # %74s  #\n', datestr(now));
+        fprintf(fid, 'Date: %74s  #\n', datestr(now));
+        fprintf(fid, '\n')
+        fprintf(fid, 'This report was generated using the data matrix from the input. The mean vector $\hat{\mu}$ and ')
+        fprintf(fid, 'covariance matrix $\hat{\Sigma}$ are obtained from the functions **estimate_mean** and ')
+        fprintf(fid, '**estimate_covariance** in the **Norm2d** class respectively.')
     fclose(fid);
 
 end
