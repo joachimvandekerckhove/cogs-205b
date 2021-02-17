13e8f55f5afb12e3800c651629cf931f469cee5d
ManuelVU <jesus_mvu@hotmail.com>
2021-02-16 19:09:11 -0600

    adding result mean to report

--- assignment/deadpool/report.m
+++ assignment/deadpool/report.m
@@ -6,12 +6,16 @@ function report(datos)
     % Create md File with results
     fid = fopen('deadpool-1.md','w');
         fprintf(fid, '# Report: Mean and Covariance matrix of the input data usin class Norm2d \n');
-        fprintf(fid, '\n')
+        fprintf(fid, '\n');
         fprintf(fid, 'Date: %74s  #\n', datestr(now));
-        fprintf(fid, '\n')
-        fprintf(fid, 'This report was generated using the data matrix from the input. The mean vector and ')
-        fprintf(fid, 'covariance matrix are obtained from the functions **estimate_mean** and ')
-        fprintf(fid, '**estimate_covariance** in the **Norm2d** class respectively.')
+        fprintf(fid, '\n');
+        fprintf(fid, 'This report was generated using the data matrix from the input. The mean vector and ');
+        fprintf(fid, 'covariance matrix are obtained from the functions **estimate_mean** and ');
+        fprintf(fid, '**estimate_covariance** in the **Norm2d** class respectively. The estimated quantities are:');
+        fprintf(fid, '\n');
+        fprintf(fid, '### The estimated quantities are:');
+        fprintf(fid, '\n');
+        fprintf(fid, '- mean = (%f, %f)',result.Mean.');
     fclose(fid);
 
 end
