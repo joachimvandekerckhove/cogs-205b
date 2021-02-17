bea7f4aba2bfa5f7aaa17ebdf163721c2675439b
doreen hii <doreen.hii@uci.edu>
2021-02-17 10:40:55 -0800

    update contents

--- assignment/groot/+groot/contents.m
+++ assignment/groot/+groot/contents.m
@@ -1,12 +1,16 @@
-function contents(obj)
-%CONTENTS Prints help text for all functions 
-    
-    % Get all methods related to class 
-    methods_list = methods(obj);
-    
-    % Disp all help functions
-    for i = 1:length(methods_list)
-        help (strcat('groot.Norm2d.', methods_list{i}));
-    end
-end
-
+% @NORM2D
+%
+% Files
+%   cdf      - Cumulative distribution function for Norm2d
+%   deviance - DEV Calculate deviance (summed logpdf of all points in xax)
+%   estimate - Takes 2xN matrix and output Norm2d object
+%   getData  - Downloads data from url
+%   logcdf   - CDF Log-Cumulative distribution function for Norm2d
+%   logpdf   - Log-Probability density function for Norm2d
+%   main     - Generate report for data http://cidlab.com/files/cogs205b.csv 
+%   Norm2d   - A class for the bivariate normal distribution
+%   pdf      - Probability density function for Norm2d
+%   readData - Read data in file into variables
+%   report   - Generate a markdown file with basic info on the input data
+%   rnd      - RNG Random number generation for Norm2d
+%   test     - Basic test suite of functions in package
