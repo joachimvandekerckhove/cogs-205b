b322530a1e4510eaeaf26fe4148fafffe864226b
ManuelVU <jesus_mvu@hotmail.com>
2021-02-16 21:31:01 -0600

    fix sample size on report

--- assignment/deadpool/report.m
+++ assignment/deadpool/report.m
@@ -9,7 +9,7 @@ function report(datos)
 
     % Get sample size by size of matrix
     n_sample = size(result)
-    n_sample = n_sample(2)
+    n_sample = n_sample(1)
 
     % Create md File with results
     fid = fopen('deadpool-1.md','w');
