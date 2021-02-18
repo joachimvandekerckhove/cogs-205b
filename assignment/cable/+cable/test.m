f2e384eaad413515618691555545baef230d0d0c
Brandon Hackney <bhackney@uci.edu>
2021-02-17 22:22:09 -0800

    Update test.m

--- assignment/cable/+cable/test.m
+++ assignment/cable/+cable/test.m
@@ -9,9 +9,10 @@ function test
         A = Norm2d();
     catch
         if ~exist('A','var')
-            fprintf("Creation of Norm2d object failed.\n")
+            fprintf("\tCreation of Norm2d object failed.\n")
+            errorCount = errorCount + 1;
         end
-        errorCount = errorCount + 1;
+        
     end
 
     B = A;
@@ -19,12 +20,12 @@ function test
     try
         B.Mean = [5;1]; B.Covariance = 5*eye(2);
     catch
-        if B.Mean == A.Mean
-            fprintf(1,"Could not update mean of Norm2d object.\n");
+        if isequal(B.Mean, A.Mean)
+            fprintf(1,"\tCould not update mean of Norm2d object.\n");
             errorCount = errorCount + 1;
         end
-        if B.Covariance == A.Covariance
-            fprintf(1,"Could not update covariance of Norm2d object.\n");
+        if isequal(B.Covariance, A.Covariance)
+            fprintf(1,"\tCould not update covariance of Norm2d object.\n");
             errorCount = errorCount + 1;
         end
     end
@@ -32,24 +33,33 @@ function test
     % Ensure you get the right size data with getData
     testData = getData('http://www.cidlab.com/files/cogs205b.csv');
     if ~exist('testData','var')
-        fprintf(1,"Could not successfully fetch data.\n");
+        fprintf(1,"\tCould not successfully fetch data.\n");
         errorCount = errorCount + 1;
     elseif ~isequal(size(testData),[2,10])
-        fprintf(1,"Could not download correct data.\n");
+        fprintf(1,"\tCould not download correct data.\n");
         errorCount = errorCount + 1;
     end
     
-    % Ensure you can run the whole thing
-    % This is the laziest test but it's all I could think of
+    % Ensure the estimate function works with the above test data
     try
-        main
-    catch errorCode
-        if exist(errorCode, 'var')
-            fprintf("Error on running main code\n");
+        C = A.estimate(testData);
+    catch
+        if isequal(C.Mean, A.Mean) && isequal(C.Covariance, A.Covariance)
+            fprintf("\tCould not estimate new distribution from test data.\n");
             errorCount = errorCount + 1;
         end
     end
     
+    % Ensure you can run the main code, which basically does all the above.
+    % If this errors but none of the above do, the problem is with main.
+    try
+        main
+    catch errorCode
+        if exist(errorCode, 'var')
+            fprintf("\tError on running main code.\n");
+            errorCount = errorCount + 1;
+        end
+    end
     
     % Final accounting
     if errorCount == 0
@@ -57,7 +67,4 @@ function test
     else
         fprintf(1,'Test returned %i errors.\n\n\n',errorCount);
     end
-    
-
-    
 end
