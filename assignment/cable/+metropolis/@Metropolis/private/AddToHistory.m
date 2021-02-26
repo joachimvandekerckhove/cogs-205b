df347a02a97c8073ffb535b09db5972292d1bca0
Brandon Hackney <bhackney@uci.edu>
2021-02-26 15:18:40 -0800

    Add initial files for metropolis

---
+++ assignment/cable/+metropolis/@Metropolis/private/AddToHistory.m
@@ -0,0 +1,8 @@
+function AddToHistory(obj)
+
+% Adds the current point to the history of points XHistory, and adds the
+% current function value to the history of function values YHistory
+
+            error('Not yet implemented')
+
+end
---
+++ assignment/cable/+metropolis/@Metropolis/private/DecideAccept.m
@@ -0,0 +1,8 @@
+function DecideAccept(obj)
+
+% Sets the "Accept" property to true if the proposed point should be
+% accepted, and to false otherwise 
+
+            error('Not yet implemented')
+            
+end
