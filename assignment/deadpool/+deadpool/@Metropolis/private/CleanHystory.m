75ab8ea3b83c3ad285679a6489fcfe23006877d8
ManuelVU <jesus_mvu@hotmail.com>
2021-03-02 23:43:12 -0600

    adding burnin property

---
+++ assignment/deadpool/+deadpool/@Metropolis/private/CleanHystory.m
@@ -0,0 +1,6 @@
+function [X , Y , I] = CleanHystory()
+%CleanHystory - Returns results without BurnIn perdod
+    X = obj.XHistory(:, obj.BurnIn+1 : size(obj.XHistory,2))
+    Y = obj.YHistory(obj.BurnIn+1 : size(obj.YHistory))
+    I = obj.BurnIn+1 : size(obj.YHistory)
+end
