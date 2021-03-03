f1a7174af914907370ab6a4812f016cb1cc578e7
ManuelVU <jesus_mvu@hotmail.com>
2021-03-02 23:45:29 -0600

    fix type

--- assignment/deadpool/+deadpool/@Metropolis/private/CleanHistory.m
+++ assignment/deadpool/+deadpool/@Metropolis/private/CleanHistory.m
@@ -1,4 +1,4 @@
-function [X , Y , I] = CleanHystory()
+function [X , Y , I] = CleanHistory()
 %CleanHystory - Returns results without BurnIn perdod
     X = obj.XHistory(:, obj.BurnIn+1 : size(obj.XHistory,2))
     Y = obj.YHistory(obj.BurnIn+1 : size(obj.YHistory))
