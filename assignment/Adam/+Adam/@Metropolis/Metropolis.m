ae2ce8626622d9d810a95aa3ab57de7bf04e3dd3
Zhibin Zhou <zhibinz2@uci.edu>
2021-03-05 14:42:39 -0800

    add files created

---
+++ assignment/Adam/+Adam/@Metropolis/Metropolis.m
@@ -0,0 +1,105 @@
+classdef Metropolis < handle
+    % Metropolis  A class for the Metropolis algorithm
+    
+    % The main properties
+    properties
+                
+        InitialValues  double {mustBeReal, mustBeFinite}
+        
+        TargetLogPdf function_handle
+        
+        TransitionStd = 1
+        
+    end
+    
+    % Derived properties that need to be set internally
+    properties (SetAccess = private)
+        
+        CurrentPointX
+        CurrentPointY
+        
+        ProposedPointX
+        ProposedPointY
+        
+        XDim
+        StepCount = 0
+        Accept = true
+        
+        XHistory = []
+        YHistory = []
+        
+    end
+        
+    % Methods are functions that belong to the class
+    methods
+        
+        %%% Constructor function %%%
+        
+        % A main constructor, for a new Metropolis
+        function obj = Metropolis(TargetLogPdf, InitialValues)
+            
+            obj.TargetLogPdf   = TargetLogPdf;
+            obj.InitialValues  = InitialValues;
+            obj.XDim           = numel(InitialValues);
+            
+            obj.CurrentPointX  = obj.InitialValues;
+            
+            obj.EvaluateCurrentPoint();
+            
+            obj.AddToHistory();
+                        
+        end
+        
+        
+        %%% Display function %%%
+        
+        % Print the state of the sampler to screen
+        function disp(obj)
+            
+            error('Not yet implemented: disp()')
+            
+        end
+        
+        
+        % Sample function
+        
+        function DrawSamples(obj, R)
+            
+            % Draws R samples from the target distribution
+            
+            for i = 1:R
+                
+                %  Draw a randomly selected point from the proposal
+                %  distribution
+                obj.DrawProposal();
+                obj.EvaluateProposedPoint();
+                
+                %  Compute the acceptance ratio
+                obj.ComputeLogAcceptanceRatio();
+                
+                % Decide whether to accept the proposal
+                obj.DecideAccept();
+                
+                % If the proposal should be accepted, make the proposed
+                % point the current point
+                if obj.Accept()
+                    obj.MakeProposalCurrent();
+                end
+                
+                % Add the current point to the chain
+                obj.AddToHistory();
+                
+            end
+            
+        end
+        
+        
+        function s = getSampleStatistics(obj)
+        
+            error('Not yet implemented: getSampleStatistics()')
+            
+        end
+                
+    end
+    
+end
