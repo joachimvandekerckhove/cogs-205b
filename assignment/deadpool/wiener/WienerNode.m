8e3a520d00d2680f6c42b01a7776e6c8bf67f415
ManuelVU <jesus_mvu@hotmail.com>
2021-03-05 18:25:18 -0600

    fix 1

--- assignment/deadpool/wiener/WienerNode.m
+++ assignment/deadpool/wiener/WienerNode.m
@@ -3,6 +3,7 @@ Classdef WienerNode < Node
     
     % The main property is now the data, but Data is defined in the parent
     properties
+        
     end
     
     % The main properties are the mean and standard deviation
