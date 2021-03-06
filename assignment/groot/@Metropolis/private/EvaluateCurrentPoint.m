d40665723514fc7286640d9a3a3ead0304729fe2
doreenhii <doreenhii.yj@gmail.com>
2021-03-05 17:07:47 -0800

    Upload assignment (#8)
    * Sync repo
    
    * Metropolis part 1
    
    * diffusion model pdfKernel

---
+++ assignment/groot/@Metropolis/private/EvaluateCurrentPoint.m
@@ -0,0 +1,7 @@
+function EvaluateCurrentPoint(obj)
+
+% Computes the target function at the current point X and stores the result
+% in current point Y.
+    obj.CurrentPointY = obj.TargetLogPdf(obj.CurrentPointX);
+            
+end 
