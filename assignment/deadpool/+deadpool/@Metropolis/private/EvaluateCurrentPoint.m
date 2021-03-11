5139900c41b440377dbe223e301125735b8466ba
ManuelVU <jesus_mvu@hotmail.com>
2021-03-10 19:10:18 -0600

    change evaluate current point metropolis class to take a column vector

--- assignment/deadpool/+deadpool/@Metropolis/private/EvaluateCurrentPoint.m
+++ assignment/deadpool/+deadpool/@Metropolis/private/EvaluateCurrentPoint.m
@@ -2,5 +2,5 @@ function EvaluateCurrentPoint(point)
 % EvaluateCurrentPoint - targetlogpdf at x
 
     % Compute the ln of the posterior distribution at current point.
-    obj.PointY = obj.TargetLogPdf(point);
+    obj.PointY(:,1) = obj.TargetLogPdf(point(:,1));
 end
