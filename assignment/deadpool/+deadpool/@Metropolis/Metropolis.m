ac73999104c1556bda7601494ed663830f796c8e
ManuelVU <jesus_mvu@hotmail.com>
2021-03-10 20:18:20 -0600

    fix sample statistic names

--- assignment/deadpool/+deadpool/@Metropolis/Metropolis.m
+++ assignment/deadpool/+deadpool/@Metropolis/Metropolis.m
@@ -117,7 +117,7 @@ classdef Metropolis < handle
             sigmahat  = std(Posteriors , [] , 2);
             quantiles = quantile(Posteriors,[0.025,0.25,0.5,0.75,0.975],2);
             s = table(muhat,sigmahat,quantiles);
-            s.Properties.VariableNames = {'Mean','SD','2.5%','25%','50%','75%','97.5%'};
+            s.Properties.VariableNames = {'Mean','SD','2.5% , 25% , 50% , 75% , 97.5%'};
             a = sym('theta_%d',[obj.XDim , 1]);
             s.Properties.RowNames = a;
         end
