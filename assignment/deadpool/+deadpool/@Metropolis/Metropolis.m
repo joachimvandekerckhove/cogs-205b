ca056ca2efcfa118e425a08b1a47dae398032da2
ManuelVU <jesus_mvu@hotmail.com>
2021-03-10 19:30:07 -0600

    change evaluate current fix 2

--- assignment/deadpool/+deadpool/@Metropolis/Metropolis.m
+++ assignment/deadpool/+deadpool/@Metropolis/Metropolis.m
@@ -57,7 +57,7 @@ classdef Metropolis < handle
             
             obj.PointX  = obj.InitialValues;
             
-            obj.PointY = obj.EvaluateCurrentPoint(); 
+            obj.EvaluateCurrentPoint(); 
             
             obj.AddToHistory();
         end
--- assignment/deadpool/+deadpool/@Metropolis/private/EvaluateCurrentPoint.m
+++ assignment/deadpool/+deadpool/@Metropolis/private/EvaluateCurrentPoint.m
@@ -1,6 +1,6 @@
-function dy = EvaluateCurrentPoint()
+function EvaluateCurrentPoint(obj)
 % EvaluateCurrentPoint - targetlogpdf at x
 
     % Compute the ln of the posterior distribution at current point.
-    dy = obj.TargetLogPdf(obj.PointX);
+    obj.PointY = obj.TargetLogPdf(obj.PointX);
 end
