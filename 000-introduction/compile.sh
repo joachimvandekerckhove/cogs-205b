55b4aaef136788a7dd3dd15696861dd18ae921bb
Joachim Vandekerckhove <joachim@uci.edu>
2021-12-31 23:27:36 -0800

    Add compile scripts

---
+++ 000-introduction/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pandoc -t dzslides -s 000-introduction.md -o 000-introduction.htm
---
+++ 100-matlab-code-organization/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pandoc -t dzslides -s 000-matlab-code-organization.md -o 000-matlab-code-organization.htm
+
+pandoc -s 101-Norm2d.md -o 101-Norm2d.htm
