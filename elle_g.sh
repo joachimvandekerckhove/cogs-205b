aad46d289cc1a271cb3383cf5dab674b22d7e34b
Elle Giovanni <emgiovan@uci.edu>
2022-01-08 19:01:46 -0800

    did it work

---
+++ elle_g.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/joachimvandekerckhove/cogs-205b.git
+git branch branchofelle
+git checkout branchofelle
+cd assignment
+mkdir elle_g
+cd ..
+cp elle_g.sh assignment/elle_g
+cd assignment/elle_g
+git add elle_g.sh
+git commit -m unix assignment, push update to git repo
+git push
