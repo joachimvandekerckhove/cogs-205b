d7eb936fa01f5ae4447657bb5c4844ef5c439e29
Elle Giovanni <emgiovan@uci.edu>
2022-01-12 18:10:57 -0800

    added branch push

--- assignment/elle_g/elle_g.sh
+++ assignment/elle_g/elle_g.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/joachimvandekerckhove/cogs-205b.git
 git branch branchofelle
+git push -u origin branchofelle
 git checkout branchofelle
 cd assignment
 mkdir elle_g
@@ -7,5 +8,5 @@ cd ..
 cp elle_g.sh assignment/elle_g
 cd assignment/elle_g
 git add elle_g.sh
-git commit -m unix assignment, push update to git repo
+git commit -m "unix assignment, push update to git repo"
 git push
