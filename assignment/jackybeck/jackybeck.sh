c4e5c99f431b3f6c2adacf14980abe7608331375
jackybeck <beckj2@uci.edu>
2022-01-13 10:45:40 -0800

    week 1 assignment

---
+++ assignment/jackybeck/jackybeck.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Assignment1
+
+git config --global user.email "beckj2@uci.edu"
+git config --global user.name "jackybeck"
+
+pseudo=jackybeck
+
+git clone git@github.com:$pseudo/cogs-205b.git
+
+cd cogs-205b/
+git checkout -b week1
+git add .
+git status
+git commit
+cd assignment
+mkdir $pseudo/
+cd ../../
+cp $pseudo.sh cogs-205b/assignment/$pseudo
+cd cogs-205b/
+git commit -a -m "week 1 assignment"
+git push --set-upstream origin small-edits
+
