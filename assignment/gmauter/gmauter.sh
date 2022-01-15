2a8f8fdc9409330a1e724a2781d04a5e9f9b543e
Garrett Michael Mauter <gmauter@uci.edu>
2022-01-14 17:19:31 -0800

    updated variable

--- assignment/gmauter/gmauter.sh
+++ assignment/gmauter/gmauter.sh
@@ -1,9 +1,9 @@
 PSEUDO=$gmauter
 git clone git@github.com:garrettmauter/cogs-205b.git
 git checkout -b week1
-mkdir cogs-205b/assignment/gmauter
-cp gmauter.sh cogs-205b/assignment/gmauter
-git add gmauter.sh
+mkdir cogs-205b/assignment/$PSEUDO
+cp gmauter.sh cogs-205b/assignment/$PSEUDO
+git add $PSEUDO.sh
 git commit -m 'commit-1'
 git push --set-upstream origin week1
 
