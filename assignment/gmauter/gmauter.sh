9fe1db915ed294024095acb1d4c4996167f63e70
Garrett Michael Mauter <gmauter@uci.edu>
2022-01-14 17:16:31 -0800

    commit-1

---
+++ assignment/gmauter/gmauter.sh
@@ -0,0 +1,10 @@
+PSEUDO=$gmauter
+git clone git@github.com:garrettmauter/cogs-205b.git
+git checkout -b week1
+mkdir cogs-205b/assignment/gmauter
+cp gmauter.sh cogs-205b/assignment/gmauter
+git add gmauter.sh
+git commit -m 'commit-1'
+git push --set-upstream origin week1
+
+
