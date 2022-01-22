e221b0e889393eb4fbaf9424724051e072c03a76
isaacmenchaca <imencha1@uci.edu>
2022-01-22 01:45:32 -0800

    up to date

---
+++ assignment/isaacmenchaca/+isaacmenchaca/@Norm2d/Norm2d.m
@@ -0,0 +1,23 @@
+classdef Norm2d
+    %NORM2D Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Property1
+    end
+    
+    methods
+        function obj = Norm2d(inputArg1,inputArg2)
+            %NORM2D Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
+
