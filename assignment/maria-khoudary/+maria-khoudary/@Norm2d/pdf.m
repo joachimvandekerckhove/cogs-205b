8de68060a1703bef1aabe232ff2fad2889bfa56f
Maria Khoudary <makhouda@uci.edu>
2022-01-25 22:20:13 -0800

    what a beast of an assignment

---
+++ assignment/maria-khoudary/+maria-khoudary/@Norm2d/pdf.m
@@ -0,0 +1,16 @@
+function yax = pdf(obj, X)
+% computes the probability density function for a bivariate normal
+% distribution
+
+% assign values to variables to improve readability & enhance flexibility
+
+sigma1=sqrt(obj.Covariance(1,1));
+sigma2=sqrt(obj.Covariance(2,2));
+
+rho=obj.Correlation;
+
+yax = (1/(2*pi*sigma1*sigma2*sqrt(1-rho^2))) ...
+    *exp((-1/2).*(obj.standardize(X)/(1-(rho^2))));
+
+end
+
