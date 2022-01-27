3d7b57403d94018c0d12f1a070a93c5eccbdfd46
hojjatazimi <azimi.hojjat@yahoo.com>
2022-01-27 15:33:27 -0800

    updated standardized function

--- assignment/isaacmenchaca/+isaacmenchaca/@Norm2d/standardized.m
+++ assignment/isaacmenchaca/+isaacmenchaca/@Norm2d/standardized.m
@@ -2,8 +2,12 @@ function z = standardized(obj, X)
         
     x1 = (X(1,:));
     x2 = (X(2,:));
-    % could define mu1 and mu2 simmilar to x1 and x2
-    z = ((x1 - obj.Mean(1)) / obj.sigma1) .^ 2 ...
-         - 2 * obj.Correlation * ((x1 - obj.Mean(1)) / obj.sigma1) .* ((x2 - obj.Mean(2)) / obj.sigma2) ...
-            + ((x2 - obj.Mean(2)) / obj.sigma2) .^ 2;
+
+    mu1 = obj.Mean(1);
+    mu2 = obj.Mean(2);
+
+    
+    z = ((x1 - mu1) / obj.sigma1) .^ 2 ...
+         - 2 * obj.Correlation * ((x1 - mu1) / obj.sigma1) .* ((x2 - mu2) / obj.sigma2) ...
+            + ((x2 - mu2) / obj.sigma2) .^ 2;
 end
