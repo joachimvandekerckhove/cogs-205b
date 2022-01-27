4c67c2c0a6997ffbef07c2792c6923e3986a81d6
lemontgomery2 <lmontgo1@uci.edu>
2022-01-27 15:39:51 -0800

    similar bit of code between functions

--- assignment/mariakhoudary/+mariakhoudary/@Norm2d/cdf.m
+++ assignment/mariakhoudary/+mariakhoudary/@Norm2d/cdf.m
@@ -3,6 +3,7 @@ function yax = cdf(obj, X)
 % using the mvncdf() function from the Statistics & Machine Learning
 % toolbox
 
+% this function is similar to the one in logcdf
 if size(X, 2) ~= size(obj.Mean, 1)
     X = X';
 end
