323755632ea216bab1285b8a2adfd43e8f591f26
Joachim Vandekerckhove <joachim@uci.edu>
2022-01-30 01:39:35 -0800

    Tidy up cdf

--- assignment/xavier/+xavier/@Norm2d/cdf.m
+++ assignment/xavier/+xavier/@Norm2d/cdf.m
@@ -1,5 +1,5 @@
 % Cumulative distribution function
-function p = cdf(obj, xax)
-% Straight outta toolbox.Stats
-p = mvncdf(xax', obj.Mean', obj.Covariance)';
+
+function outputArg = cdf(obj, xax)
+outputArg = mvncdf(xax', obj.Mean', obj.Covariance)';
 end
