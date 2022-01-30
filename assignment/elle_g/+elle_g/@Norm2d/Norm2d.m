de6bf2f20e8af1e02a993f5514ee7d23331d139c
angelaashen <33758445+angelaashen@users.noreply.github.com>
2022-01-30 02:39:05 -0800

    made requested changes, thank you fro helpful comment

--- assignment/elle_g/+elle_g/@Norm2d/Norm2d.m
+++ assignment/elle_g/+elle_g/@Norm2d/Norm2d.m
@@ -196,14 +196,9 @@ end
 
       %validator for covariance values
         function covarianceValueCheck(Covariance)
-            if~chol(Covariance)
+            [~,bool] = chol(Covariance);
+            if bool == 1 || ~(Covariance(1,2) == Covariance(2,1))
                 % cholesky factorization checks for positive definite and symmetry
-                % before you checked whether Covariance(1,2) == Covariance
-                % (2,1) but might want to make sure Covariance(1,1) ==
-                % Covariance(2,2) too :). that can be done with
-                % issymmetric() which doesn't check for positive definite 
-                % but does check for symmetry, or with chol() which checks
-                % both.
                 eidType = 'covarianceValueCheck:notcovarianceValueCheck';
                 msgType = 'The covariance values row 1 col 2 and row 2 col 1 of the covariance matrix must be equal.';
                 throwAsCaller(MException(eidType,msgType))
