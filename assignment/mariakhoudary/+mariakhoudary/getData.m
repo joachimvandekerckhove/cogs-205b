e6825b4c6cb36636be15a2c53778532adf45b924
Maria Khoudary <makhouda@uci.edu>
2022-01-31 16:19:14 -0800

    add estimate method, move getData into package dir

---
+++ assignment/mariakhoudary/+mariakhoudary/getData.m
@@ -0,0 +1,21 @@
+function getData(URL)
+
+% Downloads a CSV from the internet & returns the name of the local
+% file. Takes one input: the URL of the file to be downloaded (must be a
+% string).
+
+% Presently, the script take as input arrays, matrices, and tables. 
+% It outputs a CSV into the present working directory.
+
+% Future versions may allow the user to specify the output filetype as well
+% as download directory.
+
+data = webread(URL);
+
+if string(class(data)) == 'table'
+    writetable(data, 'downloaded_data.csv');
+else
+    writematrix(data, 'downloaded_data.csv');
+end
+
+disp(['Download successful! The file can be found at: ', pwd filesep 'downloaded_data.csv']);
