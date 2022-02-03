ebb2795d3f7a28d71e1d5a140ba3f50a4eddf67f
Adriana Chavez <achavezd@uci.edu>
2022-02-03 00:46:04 -0800

    Correcting typo

--- assignment/adri/+adri/@Norm2d/private/isValidSupport.m
+++ assignment/adri/+adri/@Norm2d/private/isValidSupport.m
@@ -1,6 +1,6 @@
 % Check for the log kernel function
-function isValidSupport(~, xax)
-    if size(xax,1)~=2 || ~all(isreal(xax))
+function isValidSupport(~, x)
+    if size(x,1)~=2 || ~all(isreal(x))
         errorId  = 'Invalid Support';
         errorMsg = 'Support should be a real-valued 2-by-N matrix.';
         throwAsCaller(MException(errorId,errorMsg))
