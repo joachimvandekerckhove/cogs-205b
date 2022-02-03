bf6c13684ceaa417b7c87175e63d91a1d3c2ba36
CNClab <ucineuralcomputation@gmail.com>
2022-02-03 09:13:13 -0800

    update getData.m

--- assignment/gmauter/+gmauter/getData.m
+++ assignment/gmauter/+gmauter/getData.m
@@ -1,3 +1,9 @@
 function output = getData(url)
 
+% saves data from http://cidlab.com/files/cogs205b.csv
+
+url_str = strsplit(url, '/'); % splits url into cell array
+file_name = url_str{end}; % gets last vector in array (csv file)
+output = websave(file_name, url) % saves data from url
+
 end
