6fe0e6d9a55d014847965b995e8a58c06898f6bf
CNClab <ucineuralcomputation@gmail.com>
2022-02-03 10:48:45 -0800

    this is so cool, never made one of these before

--- assignment/gmauter/+gmauter/Contents.m
+++ assignment/gmauter/+gmauter/Contents.m
@@ -1,1 +1,9 @@
-% Contents
+% +GMAUTER
+%
+% getData - gets data from http://cidlab.com/files/cogs205b.csv
+% readData - reads in content from url
+% report - creates markdown file which displays mean and covariance
+% test - runs basic test suite of functions in package
+% main - runs through package, creates markdown file
+
+
