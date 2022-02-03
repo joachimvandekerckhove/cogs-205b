9ccc5d7d682f7ea37485fd27479d7f78a5eb47f7
Adriana Chavez <achavezd@uci.edu>
2022-02-03 10:38:37 -0800

    Add readData.m function. Reads datafile, checks for Nx2 and stores content as a matrix.

---
+++ assignment/adri/readData.m
@@ -0,0 +1,17 @@
+function data = readData(path)
+%  Reading data from a csv file with header = True. Data must be Nx2
+    % Get file name
+    [filepath,name,ext] = fileparts(path);
+    filename = append(name,ext);
+    % Could the file been found?
+    if ~exist(path,'file')
+        error(append('File ', filename, ' could not be found'), path);
+    end
+    contenido = readtable(path,'HeaderLines', 1);
+    % Check if file contains a Nx2 dataframe.
+    if ~(width(contenido)==2)
+        error('The file does not contain 2 columns')
+    end
+    % Transform into a matrix and save
+    dat = contenido{:,:};    
+end
