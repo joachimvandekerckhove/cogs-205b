ffe09e91e6971f81cd1d68beefc63e1387d3ea36
Adriana Chavez <achavezd@uci.edu>
2022-02-03 00:52:14 -0800

    Remove standardize function

--- assignment/adri/+adri/@Norm2d/Norm2d.m
+++ assignment/adri/+adri/@Norm2d/Norm2d.m
@@ -55,20 +55,6 @@ classdef Norm2d
             obj = updateMatrix(obj);
         end
         
-        %%% Computation functions %%%
-        % Standardize a variate %
-        function z = standardize(obj,x)
-            x1 =  x(1)
-            x2 =  x(2)
-            mu1 = obj.Mean(1)
-            mu2 = obj.Mean(2)
-            sigma1 = sqrt(obj.Covariance(1,1))
-            sigma2 = sqrt(obj.Covariance(2,2))
-            rho = obj.Correlation;
-            z = ((x1-mu1)/sigma1)^2 - (2*rho)*(((x1-mu1)/sigma1) * ...
-                ((x2-mu2)/sigma2)) + ((x2-mu2)/sigma2)^2;
-        end
-        
     end
     
     
