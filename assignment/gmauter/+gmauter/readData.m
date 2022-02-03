f4e52c0791642a896a971ca30c9bddaeeb4ae320
CNClab <ucineuralcomputation@gmail.com>
2022-02-03 09:51:14 -0800

    update report.m

--- assignment/gmauter/+gmauter/readData.m
+++ assignment/gmauter/+gmauter/readData.m
@@ -1,2 +1,7 @@
-function output = readData(filename)
+function output = readData(file_name)
+
+% reads in content from url
+
+output = readmatrix(file_name)
+
 end
--- assignment/gmauter/+gmauter/report.m
+++ assignment/gmauter/+gmauter/report.m
@@ -1,2 +1,19 @@
 function report(data)
+
+% creates markdown file which displays mean and covariance
+
+fid = fopen('gmauter.md', 'w')
+fprintf(fid, 'Data from cogs205b.csv\n')
+fprintf(fid, '%s\n\n', datetime('now'))
+
+Mean = gmauter.Norm2d().estimate(X).Mean
+Covariance = gmauter.Norm2d().estimate(X).Mean
+
+
+fprintf(fid, '- Mean: ')
+fprintf(fid, '%4.2f %8.4f\n', Mean)
+fprintf(fid, '- Covariance: ')
+fprintf(fid, '%4.2 %8.4', Covariance)
+fclose(fid)
+
 end
