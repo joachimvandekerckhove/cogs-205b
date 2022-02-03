012122561ee67b1748800799985b35a6bf6de7f7
hojjatazimi <azimi.hojjat@yahoo.com>
2022-02-02 23:53:07 -0700

    removed url2name.m

--- assignment/hojjatazimi/+hojjatazimi/url2name.m
+++
@@ -1,8 +0,0 @@
-function name = url2name(url)
-    matches = strfind(url, '/');
-    url_length = length (url);
-    matches_length = length(matches);
-    final_match = matches(matches_length);
-
-name = url(final_match + 1 :url_length);
-end
