fecc494aff0dc00aaf9c6c4be0735f95c6c75a5a
Elle Giovanni <emgiovan@uci.edu>
2022-02-02 22:46:13 -0800

    fixed main function and estimate

--- assignment/elle_g/+elle_g/@Norm2d/main.m
+++ assignment/elle_g/+elle_g/@Norm2d/main.m
@@ -1,9 +1,8 @@
 %report file for data from url
-function reportFile = main()
-    % report file named pseudonym-1.md that contains the mean and covariance 
-    % of the bivariate data that are contained in http://cidlab.com/files/cogs205b.csv at the time it was run.
-    gd=elle_g.Norm2d(run, getData, 'http://cidlab.com/files/cogs205b.csv');
-    rd=elle_g.Norm2d(run, readData, gd);
-    reportFile=elle_g.Norm2d(run, report, rd);
+function main()
+    url='http://cidlab.com/files/cogs205b.csv';
+    file=elle_g.getData(url);
+    data=elle_g.readData(file);
+    elle_g.report(data);
 end
  
