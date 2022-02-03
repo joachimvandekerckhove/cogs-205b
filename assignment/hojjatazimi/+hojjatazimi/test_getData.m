8930b9e9af535449c7009a3f3a808891892b2f3b
hojjatazimi <azimi.hojjat@yahoo.com>
2022-02-02 23:49:59 -0700

    test_getData.m

---
+++ assignment/hojjatazimi/+hojjatazimi/test_getData.m
@@ -0,0 +1,28 @@
+function test_getData
+%TEST_GETDATA Summary of this function goes here
+%   Detailed explanation goes here
+good_url = 'http://cidlab.com/files/cogs205b.csv';
+try
+    file = hojjatazimi.getData(good_url);
+    if isfile(file)
+        disp('test successful')
+    else
+        error('file not found!')
+    end
+catch
+    error(['unexpected error for input: ' good_url])
+end
+bad_url(5, 'non-string not detected')
+bad_url('abcd', 'no http not detected')
+bad_url('http', 'no / not detected')
+bad_url('http/', 'no : not detected')
+end
+
+function bad_url(url, er)
+try
+    hojjatazimi.getData(url)
+    error(er)
+catch
+    disp(["error detected succesfully for " string(url)])
+end
+end
