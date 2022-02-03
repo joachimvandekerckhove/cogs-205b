96dffe2e2b2201245094c1a170f12e485b182ba4
hojjatazimi <azimi.hojjat@yahoo.com>
2022-02-03 09:50:57 -0700

    add test.m

---
+++ assignment/hojjatazimi/+hojjatazimi/test.m
@@ -0,0 +1,11 @@
+function [outputArg1,outputArg2] = test(inputArg1,inputArg2)
+%TEST Summary of this function goes here
+%   Detailed explanation goes here
+hojjatazimi.test_estimate()
+hojjatazimi.test_getData()
+hojjatazimi.test_readData()
+hojjatazimi.test_report()
+
+disp('All tests passed!')
+end
+
