c50c039ca263b80b3c7f08855c0f26c1e4d8e737
CNClab <ucineuralcomputation@gmail.com>
2022-02-03 10:41:31 -0800

    very glad I checked slack for advice

--- assignment/gmauter/+gmauter/test.m
+++ assignment/gmauter/+gmauter/test.m
@@ -5,13 +5,54 @@ function test()
 
 url = 'http://cidlab.com/files/cogs205b.csv'
 
+try
+    file_name = gmauter.getData(url)
+    errorThrown = false;
+catch
+    errorThrown = true;
+end
 
-file_name = gmauter.getData(url)
+assertErrorThrown(errorThrown, 'getData . failed to get data from URL: check URL')
 
-X = gmauter.readData(file_name)
+try
+    X = gmauter.readData(file_name)
+    errorThrown = false;
+catch
+    errorThrown = true;
+end
+assertErrorThrown(errorThrown, 'readData . failed to read content from URL')
 
-gmauter.Norm2d().estimate(X)
 
-gmauter.report(X)
 
+
+try
+    gmauter.Norm2d().estimate(X)
+    errorThrown = false;
+catch
+    errorThrown = true;
+end
+assertErrorThrown(errorThrown, 'estimate . failed to estimate mean and covariance matrix')
+
+
+
+try
+    gmauter.report(X)
+    errorThrown = false;
+catch
+    errorThrown = true;
+end
+assertErrorThrown(errorThrown, 'report . report failed')
+
+
+    % Assert that an error was thrown on a call
+    function assertErrorThrown(a,condition) % from testSuiteForNorm2d.m
+        if a
+            success(condition)
+        else
+            fprintf('#%s#\n', dashline);
+            fprintf('!  Test failed: %s\n', condition)
+            disp('!  I expected an error to be thrown')
+            throwAsCaller(failure(condition))
+        end
+    end
 end
