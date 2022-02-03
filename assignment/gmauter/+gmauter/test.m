fdddcc0433e0fa1fddc6c0a5041f24ab3d7195ac
CNClab <ucineuralcomputation@gmail.com>
2022-02-03 10:14:33 -0800

    bare bones of test - yikes

--- assignment/gmauter/+gmauter/test.m
+++ assignment/gmauter/+gmauter/test.m
@@ -1,2 +1,17 @@
 function test()
+
+% runs basic test suite of functions in package
+% command window displays test results
+
+url = 'http://cidlab.com/files/cogs205b.csv'
+
+
+file_name = gmauter.getData(url)
+
+X = gmauter.readData(file_name)
+
+gmauter.Norm2d().estimate(X)
+
+gmauter.report(X)
+
 end
