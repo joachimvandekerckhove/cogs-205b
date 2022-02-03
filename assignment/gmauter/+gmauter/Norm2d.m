fa245f91eda1f324f355bcc2151518c6b2d49e97
CNClab <ucineuralcomputation@gmail.com>
2022-02-03 12:59:29 -0800

    fix mean error

--- assignment/gmauter/+gmauter/Norm2d.m
+++ assignment/gmauter/+gmauter/Norm2d.m
@@ -90,7 +90,7 @@ classdef Norm2d
         function obj = estimate(X)
             
             Mean = mean(X, 1)
-            Cov = cov(X(1, :), X(2, :))
+            Cov = cov(X(:, 1), X(:, 2))
             obj = gmauter.Norm2d(Mean, Cov)
         end
         
