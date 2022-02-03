538b15d65fe049817d09667e5e25b5011a60a8ca
CNClab <ucineuralcomputation@gmail.com>
2022-02-03 10:20:39 -0800

    realized my report should actually be main.m

--- assignment/gmauter/+gmauter/main.m
+++ assignment/gmauter/+gmauter/main.m
@@ -1,2 +1,16 @@
 function main()
+
+% runs through package, creates markdown file
+
+url = 'http://cidlab.com/files/cogs205b.csv'
+
+file_name = gmauter.getData(url)
+
+X = gmauter.readData(file_name)
+
+gmauter.Norm2d().estimate(X)
+
+gmauter.report(X)
+
+
 end
