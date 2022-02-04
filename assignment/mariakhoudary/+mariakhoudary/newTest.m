8310e2a946c901269ccf2eec171f7971871540ca
Maria Khoudary <makhouda@uci.edu>
2022-02-03 17:10:21 -0800

    added new test suite

---
+++ assignment/mariakhoudary/+mariakhoudary/newTest.m
@@ -0,0 +1,40 @@
+function tests = newTest
+% NEWTEST uses MATLAB unit testing to run a testsuite of the mariakhoudary
+% package
+
+tests = functiontests(localfunctions);
+end
+
+%% Test that estimate() returns a Norm2d object
+function testEstimateOutput(testCase)
+    actualObj = mariakhoudary.Norm2d.estimate();
+    expectedObj = 'mariakhoudary.Norm2d';
+
+    verifyClass(testCase, actualObj, expectedObj)
+end
+
+
+%% Test estimate()'s mean function
+function testEstimateMean(testCase)
+    X = [1:4; 5:8];
+    obj = mariakhoudary.Norm2d.estimate(X);
+    correctMean1 = mean(X(1,:));
+    correctMean2 = mean(X(2,:));
+    expectedMean = vertcat(correctMean1, correctMean2);
+    actualMean = obj.Mean;
+
+    verifyEqual(testCase, actualMean, expectedMean)
+end
+
+%% Test estimate()'s input validation
+function testEstimateError(testCase)
+    Mean = [1:3; 4:6; 7:9];
+
+    badCall = @() mariakhoudary.Norm2d.estimate(Mean);
+
+    verifyError(testCase, badCall, 'Input must be 2xN')
+end
+
+
+
+
