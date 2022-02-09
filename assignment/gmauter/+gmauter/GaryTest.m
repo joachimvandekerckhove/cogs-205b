c696da70daaaff08fcc4c9e5e4ebd2d50e670e00
CNClab <ucineuralcomputation@gmail.com>
2022-02-09 14:40:10 -0800

    resolve m-lint messages and create test file

---
+++ assignment/gmauter/+gmauter/GaryTest.m
@@ -0,0 +1,24 @@
+function tests = GaryTest
+% NORMALTEST  Create test suite for Normal class
+    tests = functiontests(localfunctions);
+
+end
+
+% 3 more tests
+function testPDF(testCase)
+
+% run test
+% use verifyError at least once
+end
+
+
+
+function testSomethingElse(testCase)
+
+end
+
+
+
+function testYetAnotherThing(testCase)
+
+end
