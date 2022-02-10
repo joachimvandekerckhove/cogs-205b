9f131641ad971dd2f38958f211e5bc0103eeded4
lemontgomery2 <lmontgo1@uci.edu>
2022-02-10 15:24:12 -0800

    created file to do in class exercises

---
+++ assignment/lemontgomery2/lemontgomery2OptimizationExercises.m
@@ -0,0 +1,20 @@
+%% Optimization Exercises
+%
+
+%% Exercise 1
+% Using a hill-climbing optimization algorithm, find the maximum of 
+% $log(f(x))$ where $f(x) = e^{-x^2}$.  Compare to the maximum of $f(x)$.
+
+%% Exercise 2
+% Using a hill-climbing optimization algorithm, find the maximum of 
+% $f(x,y) = e^{-x^2-y^2}$.
+
+%% Exercise 3
+% Using some of the functions |surf|, |mesh|, |contour|, and |plot3|, graph
+% the so-called six-hump camelback function $C$: $C(x,y) = (4 - 2.1 x^2 +
+% \frac{1}{3}x^4) x^2 + x y + 4 (y^2-1) y^2$.  Then use simulated
+% annealing to find its global minimum. 
+
+%% Exercise 4
+% Read the documentation for MATLAB's |fminsearch| function.  Use it to
+% solve Exercise 2 and 3 again.
