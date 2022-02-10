1c68b341f64c78021572ac5a978cadf89fe87f4d
Adriana Chavez <achavezd@uci.edu>
2022-02-10 00:47:27 -0800

    Moving all the functions out of the +adri folder because they dont get picked by the Matlab console otherwise

--- assignment/adri/+adri/main.m
+++
@@ -1,6 +0,0 @@
-% A unction that directly executes: getData, readData and report
-function main()
-    download = getData('http://cidlab.com/files/cogs205b.csv');
-    read = readData(download);
-    report(read);
-end
