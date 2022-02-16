fbe675253c8491d3ab6784a9db56bc7e09c6d82b
Maria Khoudary <makhouda@uci.edu>
2022-02-16 11:30:00 -0800

    intialize classdef file

---
+++ assignment/mariakhoudary/+mariakhoudary/@PowerLawFitter/PowerLawFitter.m
@@ -0,0 +1,58 @@
+classdef PowerLawFitter < handle
+    % POWERLAWFITTER: A class for fitting the power law of practice rule
+
+    % Has parameters Asymptote (A), Range (B), Exposure (E), Rate (Beta)
+
+    %% Properties %%
+    % User-definable 
+    properties 
+        ObservedRT (1, :) double {mustBeReal, mustBeFinite} = ...
+            [313 306 300 293 287 288 285 281 279 275 274 273 271 272 275 268 269 265 269 264 266 264 265 264 263]
+    end
+
+    % Derived 
+    properties (SetAccess = private)
+        Count double {mustBeReal, mustBePositive, mustBeFinite, mustBeNonzero, mustBeInteger}
+        EstimatedAsymptote double {mustBeReal}
+        EstimatedRange double {mustBeReal}
+        EstimatedExposure double {mustBeReal}
+        EstimatedRate double {mustBeReal}
+    end
+
+    % Constant 
+    properties (Constant)
+        Name = 'Power Law Fitter'
+    end
+
+    %% Methods %%
+methods
+
+    %%% Constructor %%%
+    function obj = PowerLawFitter(ObservedRT)
+        if nargin > 0
+            obj.ObservedRT = ObservedRT;
+        end
+        obj = Fit();
+    end
+
+    %%% Display %%%
+    function disp(obj)
+        %%% displays Count & parameter estimates
+    end
+
+
+    %%% Computation functions %%%
+
+    % Expected reaction time
+    ERT = Expectation(obj, A, B, E, beta)
+
+    % Sum of squared errors
+    SSE = SumOfSquaredError(obj, A, B, E, beta)
+
+    % Fitter
+    fit(obj)
+
+end
+
+
+end
