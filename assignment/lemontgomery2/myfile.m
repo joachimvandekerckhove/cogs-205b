6891483cd2445f29ea98ac9a578b0fdac133a302
lemontgomery2 <lmontgo1@uci.edu>
2022-02-17 12:55:37 -0800

    deleted file

--- assignment/lemontgomery2/myfile.m
+++
@@ -1,9 +0,0 @@
-% helpful script to get identifier for MATLAB errors
-% useful when creating verifyError in test suite
-
-try
-    webread('')
-catch ME
-    disp('Error Message:')
-    disp(ME.message)
-end
