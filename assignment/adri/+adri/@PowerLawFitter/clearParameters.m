117b1e06f41a919c93896a160b652a3f2e63646f
Adriana Chavez <achavezd@uci.edu>
2022-02-20 13:14:03 -0800

    Fix: The Count property was not being updated after inserting new data

--- assignment/adri/+adri/@PowerLawFitter/clearParameters.m
+++ assignment/adri/+adri/@PowerLawFitter/clearParameters.m
@@ -1,7 +1,9 @@
-%Clear previous parameter estimates when data changes
+% When the data is changed, the parameter values previously estimated are
+% reset and the size of this new data string is registered.
 function clearParameters(obj)
     obj.EstimatedAsymptote = [];
     obj.EstimatedRange = [];
     obj.EstimatedRate = [];
-    obj.EstimatedExposure = [];    
+    obj.EstimatedExposure = [];
+    obj.Count = size(obj.ObservedRT,2);
 end
