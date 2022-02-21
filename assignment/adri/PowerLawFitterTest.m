9631549667e1cb54bfe49f361ca0d57d43f50f46
Adriana Chavez <achavezd@uci.edu>
2022-02-20 17:43:19 -0800

    Small changes: We change the names of these files

---
+++ assignment/adri/PowerLawFitterTest.m
@@ -0,0 +1,30 @@
+function tests = PowerLawFitterTest
+% Create test suite for adri.PowerLawFitter
+    tests = functiontests(localfunctions);
+end
+
+% test Infinite values
+function testInfCheck(testCase)
+    Means              = [Inf,Inf];
+    Covariance         = [1 0; 0 1];    
+    badCall = @() adri.Norm2d(Means, Covariance);    
+    verifyError(testCase, badCall, 'MATLAB:validators:mustBeFinite')
+end
+
+% test getData function
+function testGetData(testCase)
+    url = 'www.adriland.com';
+    badcall   = @() adri.getData(url);  
+    verifyError(testCase, badcall, 'MATLAB:requireValidURL')    
+end
+
+% test cdf function
+function testFx(testCase)
+    Means            = [1 7];
+    Covariance       = [5 1;1 5];
+    values           = [linspace(1,50,5);linspace(50,1,5)];
+    
+    expectedValue   = mvncdf(values',Means,Covariance)';
+    actualValue     = adri.Norm2d(Means, Covariance).cdf(values);    
+    verifyEqual(testCase, expectedValue, actualValue, "AbsTol", eps(1))
+end
