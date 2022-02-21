717bb48ebd7ba532adadb35267c4130eb6ed7b4e
Adriana Chavez <achavezd@uci.edu>
2022-02-20 17:35:26 -0800

    Fix: The SSE function also throws an error if parameter values are not specified. It wouldn't make sense to use arbitrarily default values here

--- assignment/adri/+adri/@PowerLawFitter/sse.m
+++ assignment/adri/+adri/@PowerLawFitter/sse.m
@@ -1,8 +1,19 @@
-% Computing the Sum of Squared Error
+% Sum of Squared Error
 function SSE = sse(obj,A,B,E,beta)   
+% Calculate the Sum of Squared Errors between the expected and the observed
+% mean RTs, given some parameter values.
+    
+    % If parameter values aren't specified, we can't compute expectations
+    if nargin < 2
+        error(['ERROR MESSAGE: Parameter values for the Power Law model need to be ' ...
+            'specified to compute the expected RTs against which we compare the observed data'])
+    end
+
+    %Calculate the SSE
     Observed = obj.ObservedRT;
     Expected = obj.expectedRT(A,B,E,beta);
-    error = Observed-Expected;
-    SumSq = error.^2;
+    Error = Observed-Expected;
+    SumSq = Error.^2;
     SSE = sum(SumSq);
+    
 end
