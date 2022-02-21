e4bd667105ab65154c9d13d8a2a0a64092634ea0
Adriana Chavez <achavezd@uci.edu>
2022-02-21 11:24:26 -0800

    Modify the Fit function so that it doesnt run if a problem is found with the initial values set by the user

--- assignment/adri/+adri/@PowerLawFitter/myFit.m
+++ assignment/adri/+adri/@PowerLawFitter/myFit.m
@@ -20,30 +20,45 @@ function obj = myFit(obj,A,B,E,beta)
                         end                        
                     end                    
                 end                               
-             end 
+            end 
             
+            % Check if initial values set by the user are valid
+            % before estimating parameters for any given data.
+            if nargin > 1 && isempty(obj.EstimatedAsymptote)
+                valuesFlag = obj.checkValues(A,B,E,beta);
+            else
+                valuesFlag = 0;
+            end
+                      
             %Only run the optimization process if there are no currently
-            %available parameter estimates.
-            if isempty(obj.EstimatedAsymptote)
+            %available parameter estimates nor a problem with the initial
+            %values was detected
+            line = repmat('-', 1, 73);
+            fprintf('*%s*\n', line); 
+            if isempty(obj.EstimatedAsymptote) && valuesFlag == 0
                 %Optimization!
                 objective = @(x) obj.sse(x(1),x(2),x(3),x(4));
                 options = optimset('MaxFunEvals', 1e6, 'MaxIter', 1e6);
-                [x, fval] = fminsearch(objective,[A B E beta],options);
+                [x, ~] = fminsearch(objective,[A B E beta],options);
                 %Retrieving values
                 obj.EstimatedAsymptote = x(1);
                 obj.EstimatedRange = x(2);            
                 obj.EstimatedExposure = x(3);
                 obj.EstimatedRate = x(4);            
                 % Print to screen initial values used
-                line = repmat('-', 1, 73);
-                fprintf('*%s*\n', line); 
                 fprintf("OPTIMIZATION ran using the following INITIAL VALUES:\n") 
                 fprintf("Initial A (Asymptote):    %3g %1s\n", A, Par.setBy(1))
                 fprintf("Initial B (Range):        %3g %1s\n", B, Par.setBy(2))
                 fprintf("Initial E (Exposure):     %3g %1s\n", E, Par.setBy(3))
                 fprintf("Initial beta (Rate):      %3g %1s\n", beta,Par.setBy(4)) 
             else
-                %Don't do optimization again until the data has changed
-                fprintf("Parameter values for this dataset had already been estimated.\n") 
+                if ~isempty(obj.EstimatedAsymptote)
+                    %Don't do optimization again until the data has changed
+                    fprintf("Parameter values for this dataset had already been estimated.\n") 
+                end
+                if isempty(obj.EstimatedAsymptote) && valuesFlag == 1
+                   %Stop optimization until initial values have changed
+                   fprintf("Optimization stopped: Please choose different initial values")
+                end
             end
 end
