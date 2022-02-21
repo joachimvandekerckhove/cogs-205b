0eb49dfb1b25b1b49062cae0a016ccfe45df3c69
Adriana Chavez <achavezd@uci.edu>
2022-02-20 17:34:02 -0800

    Fix: The function now throws an error if parameter values are not specified. It wouldn't make sense to compute expected RTs for arbitrary parameters

--- assignment/adri/+adri/@PowerLawFitter/expectedRT.m
+++ assignment/adri/+adri/@PowerLawFitter/expectedRT.m
@@ -1,5 +1,16 @@
-% Computed the expected mean RT
-function ERT = expectedRT(obj,A,B,E,beta)            
-    N = 1:size(obj.ObservedRT,2);
-    ERT = A + (B*(N+E).^(-beta));
+% Expected Mean Response Times
+function ERT = expectedRT(obj,A,B,E,beta)
+% Compute the expected mean Response Times for each session ran, given 
+% some parameter values
+    
+    % If parameter values aren't specified, we can't compute expectations
+    if nargin < 2
+        error(['ERROR MESSAGE: Parameter values for the Power Law model need to be ' ...
+            'specified to compute the expected mean RTs'])
+    end
+    
+    % Compute the expected mean RTs per session.
+    N = 1:obj.Count;
+    ERT = A + (B*(N+E).^(-beta));   
+
 end
