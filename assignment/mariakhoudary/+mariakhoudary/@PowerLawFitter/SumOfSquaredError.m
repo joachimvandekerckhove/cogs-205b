33fa755174bd04f49529b70df2473a00068fcbdf
Maria Khoudary <makhouda@uci.edu>
2022-02-21 23:07:13 -0800

    add end statements, make second input a vector of parameters

--- assignment/mariakhoudary/+mariakhoudary/@PowerLawFitter/SumOfSquaredError.m
+++ assignment/mariakhoudary/+mariakhoudary/@PowerLawFitter/SumOfSquaredError.m
@@ -1,8 +1,9 @@
-function SSE = SumOfSquaredError(A, B, E, beta)
+function SSE = SumOfSquaredError(obj, parameters)
 
 % SUMOFSQUAREDERROR returns the sum of squared errors between ObservedRT
 % and ExpectedRT (ERT) 
 
-ERT = obj.Expectation(A, B, E, beta);
-
+ERT = obj.Expectation(parameters);
 SSE = sum((ERT - obj.ObservedRT).^2);
+
+end
