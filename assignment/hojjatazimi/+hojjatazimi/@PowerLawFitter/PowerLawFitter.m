cc48f6be6421a734321280146c2b1b57257b1412
hojjatazimi <azimi.hojjat@yahoo.com>
2022-02-23 22:57:38 -0800

    Update PowerLawFitter.m getters for Estimates

--- assignment/hojjatazimi/+hojjatazimi/@PowerLawFitter/PowerLawFitter.m
+++ assignment/hojjatazimi/+hojjatazimi/@PowerLawFitter/PowerLawFitter.m
@@ -27,15 +27,47 @@ classdef PowerLawFitter < handle
                 obj.Count = length(ObservedRT);
             end
         end
-        function obj = set.Count(obj, value)
+        function set.Count(obj, value)
              obj.Count_ = length(obj.ObservedRT);
+        end
+        function set.ObservedRT(obj, ObservedRT)
+            obj.ObservedRT = ObservedRT;
+            obj.fit = false;
         end
         function Precision = get.ObservedRT(obj)
             Precision = obj.ObservedRT;
         end
         function Count = get.Count(obj)
             Count = length(obj.ObservedRT);
+        end       
+        function EstimatedAsymptote = get.EstimatedAsymptote(obj)
+            if obj.fit
+                EstimatedAsymptote = obj.EstimatedAsymptote;
+            else
+                error ('PowerLawFitter:getEstimatedAsymptote:mustBeFit', 'Model is not fit!')
+            end
+        end
+        function EstimatedRange = get.EstimatedRange(obj)
+            if obj.fit
+                EstimatedRange = obj.EstimatedRange;
+            else
+                error ('PowerLawFitter:getEstimatedRange:mustBeFit', 'Model is not fit!')
+            end
+        end
+        function EstimatedExposure = get.EstimatedExposure(obj)
+            if obj.fit
+                EstimatedExposure = obj.EstimatedExposure;
+            else
+                error ('PowerLawFitter:getEstimatedExposure:mustBeFit', 'Model is not fit!')
+            end
         end        
+        function EstimatedRate = get.EstimatedRate(obj)
+            if obj.fit
+                EstimatedRate = obj.EstimatedRate;
+            else
+                error ('PowerLawFitter:getEstimatedRate:mustBeFit', 'Model is not fit!')
+            end
+        end
         ERT = Expectation(obj, A, B, E, b)
         SSE = SumOfSquaredError (obj, A, B, E, b)
         void = Fit(obj)
