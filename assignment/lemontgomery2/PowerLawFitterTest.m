6d23740deac7e0d4322b6fa265bfdd2370294583
lemontgomery2 <lmontgo1@uci.edu>
2022-02-24 10:27:56 -0800

    test suite working, trying to make testDisp not display anything to console

--- assignment/lemontgomery2/PowerLawFitterTest.m
+++ assignment/lemontgomery2/PowerLawFitterTest.m
@@ -107,10 +107,13 @@ function testExpectation(testCase)
     data = [313   306   300   293   287   288   285   281   279   ...
             275   274   273   271   272   275   268   269   265   ...
             269   264   266   264   265   264   263];
-
-    actualValue   = @() lemontgomery2.report(data);
+    x = [20, 50, 1, 0];
     
-    verifyWarningFree(testCase, actualValue)
+    this = lemontgomery2.PowerLawFitter(data);
+    actualValue   = this.expectation(x);
+    actualSize    = [1 25];
+    
+    verifySize(testCase, actualValue, actualSize)
     
 end
 
@@ -120,33 +123,40 @@ function testSSE(testCase)
     data = [313   306   300   293   287   288   285   281   279   ...
             275   274   273   271   272   275   268   269   265   ...
             269   264   266   264   265   264   263];
-
-    actualValue   = @() lemontgomery2.report(data);
+    testData = randperm(313, 25);
     
-    verifyWarningFree(testCase, actualValue)
+    this = lemontgomery2.PowerLawFitter(data);
+    actualValue   = this.sse(testData);
+        
+    verifyGreaterThanOrEqual(testCase, actualValue, 0)
     
 end
 
 function testFit(testCase)
 
     % TESTFIT    Tests Fit function with valid input to check if parameter
-    % estimates updated
-    data = [5.4694 -3.3563 1.1302 3.2101 5.4024 7.1768 1.3437 -4.9664 -1.9692 -3.2463; ...
-            10.4018 -1.4624 3.9923 0.2303 4.5544 -2.0594 -4.6091 -4.6895 2.9528 0.2905]';
+    % estimate updated
+    data = [313   306   300   293   287   288   285   281   279   ...
+            275   274   273   271   272   275   268   269   265   ...
+            269   264   266   264   265   264   263];
 
-    actualValue   = @() lemontgomery2.report(data);
+    this = lemontgomery2.PowerLawFitter(data);
+    this.Fit;
+    actualValue = this.EstimatedAsymptote;
     
-    verifyWarningFree(testCase, actualValue)
+    verifyNotEmpty(testCase, actualValue)
     
 end
 
 function testDisp(testCase)
 
     % TESTDISP    Tests disp function with valid input
-    data = [5.4694 -3.3563 1.1302 3.2101 5.4024 7.1768 1.3437 -4.9664 -1.9692 -3.2463; ...
-            10.4018 -1.4624 3.9923 0.2303 4.5544 -2.0594 -4.6091 -4.6895 2.9528 0.2905]';
+    data = [313   306   300   293   287   288   285   281   279   ...
+            275   274   273   271   272   275   268   269   265   ...
+            269   264   266   264   265   264   263];
 
-    actualValue   = @() lemontgomery2.report(data);
+    this = lemontgomery2.PowerLawFitter(data);
+    actualValue = @() this.disp;
     
     verifyWarningFree(testCase, actualValue)
     
@@ -155,24 +165,39 @@ end
 % Bonus Tests
 function testBonus1(testCase)
 
-    % TESTBONUS1    Tests Fit function that should return value
-    data = [5.4694 -3.3563 1.1302 3.2101 5.4024 7.1768 1.3437 -4.9664 -1.9692 -3.2463; ...
-            10.4018 -1.4624 3.9923 0.2303 4.5544 -2.0594 -4.6091 -4.6895 2.9528 0.2905]';
-
-    actualValue   = @() lemontgomery2.report(data);
+    % TESTBONUS1    Tests Fit function that should return new value
+    data = [313   306   300   293   287   288   285   281   279   ...
+            275   274   273   271   272   275   268   269   265   ...
+            269   264   266   264   265   264   263];
+    testData = randperm(313, 25);
+        
+    this = lemontgomery2.PowerLawFitter(data);
+    this.Fit;
+    actualValue = this.EstimatedAsymptote;
     
-    verifyWarningFree(testCase, actualValue)
+    this2 = lemontgomery2.PowerLawFitter(testData);
+    this2.Fit;
+    actualValue2  = this2.EstimatedAsymptote;
+    
+    verifyNotEqual(testCase, actualValue, actualValue2)
     
 end
 
 function testBonus2(testCase)
 
     % TESTBONUS2    Tests Fit function that shouldn't return value
-    data = [5.4694 -3.3563 1.1302 3.2101 5.4024 7.1768 1.3437 -4.9664 -1.9692 -3.2463; ...
-            10.4018 -1.4624 3.9923 0.2303 4.5544 -2.0594 -4.6091 -4.6895 2.9528 0.2905]';
-
-    actualValue   = @() lemontgomery2.report(data);
+    data = [313   306   300   293   287   288   285   281   279   ...
+            275   274   273   271   272   275   268   269   265   ...
+            269   264   266   264   265   264   263];
+    testData = randperm(313, 25);
+        
+    this = lemontgomery2.PowerLawFitter(data);
+    this.Fit;
+    actualValue = this.EstimatedAsymptote;
     
-    verifyWarningFree(testCase, actualValue)
+    this2 = lemontgomery2.PowerLawFitter(testData);
+    actualValue2 = this2.EstimatedAsymptote;
+    
+    verifyNotEqual(testCase, actualValue, actualValue2)
     
 end
