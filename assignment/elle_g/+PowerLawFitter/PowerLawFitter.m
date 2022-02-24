97330225ab2655cf340c9d7b987f6ab081d2994b
Elle Giovanni <emgiovan@uci.edu>
2022-02-23 16:44:10 -0800

    fixed formatting

--- assignment/elle_g/+PowerLawFitter/PowerLawFitter.m
+++ assignment/elle_g/+PowerLawFitter/PowerLawFitter.m
@@ -3,17 +3,49 @@ classdef PowerLawFitter < handle
     % decreases according to a power law as a function of number of trials N.
     % ERT = A + B (N + E)^{-beta}
     % PARAMETERS: Asymptote A-Asymptote, B-Range, E-Exposure, Rate-beta
+    
+    %define properties
     properties
         ObservedRT double {mustBeFinite, mustBeNumeric(ObservedRT), mustBeVector(ObservedRT)}%need to make sure it is a vector
     end
     
     properties (SetAccess=private)
-        Count
         EstimatedAsymptote
         EstimatedRange
         EstimatedExposure
         EstimatedRate
     end
+    
+    properties (Dependent)
+        Count
+    end
+    
+    % getters
+    methods
+        function value = get.ObservedRT(obj)
+            value=obj.ObservedRT;
+        end
+        
+        function value = get.Count(obj)
+            value = obj.Count;
+        end
+    end
+    
+    % setters
+    methods
+        function set.Count(obj,input)
+            obj.Count=length(input);
+        end
+        function set.ObservedRT(obj, input)
+            obj.ObservedRT = input;
+            % update dependent property
+            set.Count(obj, input);
+            % update private properties
+            obj.fit();
+        end
+        
+    end
+    %methods
     
     methods
         function obj = PowerLawFitter(ObservedRT)
@@ -21,14 +53,14 @@ classdef PowerLawFitter < handle
             obj=fit();
         end
         
-        
         function ERT = Expectation(A, B, E, beta)
             A=obj.EstimatedAsymptote;
             B=obj.EstiamtedRange;
             E=obj.EstimatedExposure;
             beta=obj.EstimatedRate;
             N=obj.Count;
-
+            
+        end
         
         function SSE = SumOfSquaredError(A, B, E, beta)
             ERT=obj.Expectation(A, B, E, beta); %should it be .fit?
@@ -36,20 +68,6 @@ classdef PowerLawFitter < handle
             squaredError=error.^2;
             SSE=sum(squaredError);
             
-        end
-    end
-    
-    methods (Static)
-        function fit() %no input, no output
-            obj.Count=length(obj.ObservedRT);
-            N=obj.Count;
-            for 1:N
-                fun=@(x) A + B.*(N+E).^-beta
-                x0=mean(obj.ObservedRT);
-                x=fminsearch(fun, x0)
-            end
-            
-            % the goal of fit is to make the SSE as small as possible
         end
         
         function disp() %no input, no putput
@@ -67,8 +85,18 @@ classdef PowerLawFitter < handle
             fprintf(formSpecB, obj.EstimatedRange)
             fprintf(formSpecE, obj.EstimatedExposure)
             fprintf(formSpecBeta, obj.EstimatedRate)
-            
         end
     end
-end
-
+    
+    methods (Static)
+        function fit() %no input, no output
+            obj.Count=length(obj.ObservedRT);
+            N=obj.Count;
+            for 1:N
+                fun=@(x) A + B.*(N+E).^-beta
+                x0=mean(obj.ObservedRT);
+                x=fminsearch(fun, x0)
+            end
+        end
+    end
+    
