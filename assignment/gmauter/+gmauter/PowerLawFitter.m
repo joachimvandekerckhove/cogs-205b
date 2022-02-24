819254bec90ac25cc35a6cb71952bcdf509ed10e
CNClab <ucineuralcomputation@gmail.com>
2022-02-24 09:20:47 -0800

    add functionality to methods, still have disp and bonus to work on

--- assignment/gmauter/+gmauter/PowerLawFitter.m
+++ assignment/gmauter/+gmauter/PowerLawFitter.m
@@ -1,9 +1,7 @@
 classdef PowerLawFitter < handle
-    %UNTITLED2 Summary of this class goes here
-    %   Detailed explanation goes here
     
     properties
-        ObservedRT % row vector of observed mean reaction times over N trials
+        ObservedRT (1, :) % row vector of observed mean reaction times over N trials
     end
     
     properties (Dependent)
@@ -19,23 +17,59 @@ classdef PowerLawFitter < handle
     
     methods
         
-        function obj = Expectation(A, B, E, beta)
-         
-            %   output is ERT, a vector of expected reaction times over N
-            %   trials where N is count
-            obj = ERT
+        % constructor function
+        function obj = PowerLawFitter(RTData)
+            if nargin > 0
+                obj.ObservedRT = RTData;
+            end
         end
         
-        function outputArg = SumOfSquaredError(A, B, E, beta)
-            %   output is SSE, a scalar that is the squared difference
-            %   between ERT and ObservedRT, summed
-            outputArg = SSE
+        % get # trials in RTData
+        function count = get.Count(obj)
+            count = length(obj.ObservedRT);
         end
         
-        function Fit()
+        
+        function ERT = Expectation(obj)
+            % output is ERT, a vector of expected reaction times over N
+            % trials where N is count
+           
+            % initialize for 'speed'
+            ERT = zeros(1:obj.Count);
+            
+            % compute ERT for each index
+            for t = 1:obj.Count
+                ERT(t) = obj.EstimatedAsymptote + obj.EstimatedRange * (t + obj.EstimatedExposure)^(-obj.EstimatedRate);
+            end
+             
+        end
+        
+        function SSE = SumOfSquaredError(obj)
+            % output is SSE, a scalar that is the squared difference
+            % between ERT and ObservedRT, summed
+            ERT = Expectation(obj);
+            SSE = sum((ERT - obj.ObservedRT).^2); % shoutout Jeff Rouder
+        end
+        
+        function Fit(obj)
             % after running fit, the private access properties should be
             % set to be the estimated parameters of the power law to these
             % data
+            
+            %set arbitrary values
+            t = 1:obj.Count;
+            SSEFunction = @(p) sum(p(1)+p(2).*(t+p(3).^(-p(4))-obj.ObservedRT).^2);
+           
+            range = max(obj.ObservedRT) - min(obj.ObservedRT); % get range of RTData
+            options = optimset('MaxFunEvals', 1e6, 'MaxIter', 1e6);
+            initVals = fminsearch(SSEFunction, [mean(obj.ObservedRT), range , 1, 1], options);
+            
+            % set params
+            obj.EstimatedAsymptote = initVals(1);
+            obj.EstimatedRange = initVals(2);
+            obj.EstimatedExposure = initVals(3);
+            obj.EstimatedRate = initVals(4);
+            
             
         end
         
