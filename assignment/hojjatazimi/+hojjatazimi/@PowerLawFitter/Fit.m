ffbddd5ad89ad800f37344a50f32a6dd2e5e2890
hojjatazimi <azimi.hojjat@yahoo.com>
2022-02-23 22:46:59 -0800

    Fix Fit.m

--- assignment/hojjatazimi/+hojjatazimi/@PowerLawFitter/Fit.m
+++ assignment/hojjatazimi/+hojjatazimi/@PowerLawFitter/Fit.m
@@ -1,6 +1,7 @@
 function Fit(obj)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
+
 if obj.fit 
 disp('The model is already fit')
 else
@@ -15,5 +16,8 @@ disp(x);
 
 plot(obj.ObservedRT, '*-');hold on;
 plot(obj.Expectation(x(1), x(2), x(3), x(4)), 's-');
+
+legend('Observed Data', 'Estimated Data')
+obj.fit = true;
 end
 end
