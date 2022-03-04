8083a18c8395fac0349a5e25523d2e445e2a743b
angelaashen <angelaashen@gmail.com>
2022-03-04 11:02:37 -0800

    small fixes

--- assignment/angela/+final/ConstrainedLogPosterior.m
+++ assignment/angela/+final/ConstrainedLogPosterior.m
@@ -12,17 +12,14 @@ shape_m = shape_i + shape_s;
 scale_h = scale_i + 2*scale_s;
 shape_h = shape_i + 2*shape_s;
 
-data_e = data.easy;
-data_m = data.medium;
-data_h = data.hard;
-
-ll_e = -(wbllike([scale_e, shape_e], data_e));
-ll_m = -(wbllike([scale_m, shape_m], data_m));
-ll_h = -(wbllike([scale_h, shape_h], data_h));
+ll = @(params,data) -(wbllike(params,data));
+ll_e = ll([scale_e,shape_e],data.easy);
+ll_m = ll([scale_m,shape_m],data.medium);
+ll_h = ll([scale_h,shape_h],data.hard);
 
 ll = ll_e + ll_m + ll_h; 
 
-lp = (-sum(scale_i)) + (-abs(scale_s)) + (-sum(scale_i)) + (-abs(scale_s));
+lp = (-sum(scale_i)) + (-abs(scale_s)) + (-sum(shape_i)) + (-abs(shape_s));
 
 logposterior = ll + lp; 
 
--- assignment/angela/+final/SaturatedLogPosterior.m
+++ assignment/angela/+final/SaturatedLogPosterior.m
@@ -1,19 +1,9 @@
 function logposterior = SaturatedLogPosterior(params, data)
 
-scale_e = params(1);
-scale_m = params(2);
-scale_h = params(3);
-shape_e = params(4);
-shape_m = params(5);
-shape_h = params(6);
-
-data_e = data.easy;
-data_m = data.medium;
-data_h = data.hard; 
-
-ll_e = -(wbllike([scale_e, shape_e], data_e));
-ll_m = -(wbllike([scale_m, shape_m], data_m));
-ll_h = -(wbllike([scale_h, shape_h], data_h));
+ll = @(params,data) -(wbllike(params,data));
+ll_e = ll([params(1),params(4)],data.easy);
+ll_m = ll([params(2),params(5)],data.medium);
+ll_h = ll([params(3),params(6)],data.hard);
 
 ll = ll_e + ll_m + ll_h; 
 
