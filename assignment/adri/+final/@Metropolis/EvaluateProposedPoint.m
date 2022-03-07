aed1c640603ddc91a6c8fd78bcf982c97fc0e78e
Adriana <adrifelcha@gmail.com>
2022-03-07 15:02:03 -0800

    Adding EvaluateProposedPoint method

--- assignment/adri/+final/@Metropolis/EvaluateProposedPoint.m
+++ assignment/adri/+final/@Metropolis/EvaluateProposedPoint.m
@@ -1,8 +1,7 @@
+% Computes the target function at the proposed point X
+% X and stores the result in proposed point Y.
 function EvaluateProposedPoint(obj)
-
-% Computes the target function at the proposed point X and stores the result
-% in proposed point Y.
-
-            error('Not yet implemented')
+    
+    obj.ProposedPointY = obj.TargetLogPdf(obj.ProposedPointX);
 
 end
