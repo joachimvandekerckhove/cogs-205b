a67a7a462366ebb2c86b618338e8ee7ac7f1df42
Adriana <adrifelcha@gmail.com>
2022-03-07 12:52:48 -0800

    Adding the DrawProposal method

--- assignment/adri/+final/@Metropolis/DrawProposal.m
+++ assignment/adri/+final/@Metropolis/DrawProposal.m
@@ -1,10 +1,13 @@
-function DrawProposal(obj)
-
 % Draws a new proposed point by adding a Gaussian offset to the current
 % point.  The standard deviation of the offset is given by the
 % TransitionStd property.  If the current point is a vector, adds an
 % independent Gaussian offset to each dimension.
+function DrawProposal(obj)
 
-            error('Not yet implemented')
+% Draw a row vector (size=XDimensions) of Gaussian offsets
+    offsets = normrnd(0,obj.TransitionStd,[1, obj.XDimensions]);
+
+% Define candidate vector by adding these offsets to the current values
+    obj.ProposedPointX = obj.CurrentPointX + offsets;
             
 end
