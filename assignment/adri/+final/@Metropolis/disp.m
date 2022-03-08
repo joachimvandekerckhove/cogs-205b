a199ec6378c9387d88672e491dce4a8b44a4a1e8
Adriana Chavez <achavezd@uci.edu>
2022-03-08 14:11:57 -0800

    Moving the disp method into my folder

---
+++ assignment/adri/+final/@Metropolis/disp.m
@@ -0,0 +1,6 @@
+function disp(obj)
+
+% This should at a minimum output (a) whether parameters have been estimated and
+% (b) the mean and standard deviation of each parameter if they have been estimated.
+
+end
