f972ef61c46ea196c6870daad0cfca3e3148563f
Adriana Chavez <achavezd@uci.edu>
2022-03-08 12:20:22 -0800

    Add the AddToHistory method

--- assignment/adri/+final/@Metropolis/AddToHistory.m
+++ assignment/adri/+final/@Metropolis/AddToHistory.m
@@ -1,12 +1,14 @@
-function AddToHistory(obj)
-
 % Adds the current point to the history of points XHistory, and adds the
 % current function value to the history of function values YHistory.
 % After running this, the XHistory property should have a new column that
 % contains the most recently accepted sample, and the YHistory property
 % should have a new element that contains the corresponding target function
 % value.
+function AddToHistory(obj)
 
-            error('Not yet implemented')
+    % Add the approved Y value to the History vector of Y values
+    obj.YHistory = [obj.YHistory, obj.CurrentPointY];
+    % Add a new column with the approved X values to the X History matrix
+    obj.XHistory = [obj.XHistory, obj.CurrentPointX];           
 
 end
