e2effdb28e9a59a7524f30212bbdd2b24e89f928
Adriana Chavez <achavezd@uci.edu>
2022-03-08 11:32:48 -0800

    Add DecideAccept method, as described in the pseudocode

--- assignment/adri/+final/@Metropolis/DecideAccept.m
+++ assignment/adri/+final/@Metropolis/DecideAccept.m
@@ -1,8 +1,15 @@
+% Sets the "Accept" property to true if the proposed 
+% point should be accepted, and to false otherwise 
 function DecideAccept(obj)
 
-% Sets the "Accept" property to true if the proposed point should be
-% accepted, and to false otherwise 
-
-            error('Not yet implemented')
+    % Draw a random value u from a Uniform(0,1) and take the log
+    u = rand(1);
+    logU = log(u);    
+    % If LogAcceptanceRatio > log(u), the proposed X is accepted    
+    if obj.LogAcceptanceRatio > logU
+       obj.AcceptProposal = true;
+    else
+       obj.AcceptProposal = false;
+    end
             
 end
