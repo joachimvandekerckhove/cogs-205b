f41700caafafb79fd15524935fc5fd62aba405b1
Elle Giovanni <emgiovan@uci.edu>
2022-03-09 22:03:03 -0800

    changes to constrained and saturated log posterior functions

--- assignment/elle_g/+final/ConstrainedLogPosterior.m
+++ assignment/elle_g/+final/ConstrainedLogPosterior.m
@@ -14,4 +14,21 @@ function ConLogPost = ConstrainedLogPosterior(parameter, data)
     easy=data.easy;
     med=data.medium;
     hard=data.hard;
+    
+    easyL = -wbllike([Ae, Be], easy);
+    medL = -wbllike([Am, Bm], med);
+    hardL = -wbllike([Ah, Bh], hard);
+    logL= easyL+medL+hardL;
+    
+    % So in the six occurrences of Exp(), 
+    % just replace x by the right A and B values and lambda by 1.  
+    % Then the equation with the six Exp() in it will simplify a lot
+    
+    B0a=1.*exp(-1.*B0a);
+    B0b=1.*exp(-1.*B0b);
+    B1a=-abs(B1a);
+    B1b=-abs(B1b);
+    LogP=B0a+B0b+B1a+B1b;
+    
+    ConLogPost=logL+LogP;
 end
--- assignment/elle_g/+final/SaturatedLogPosterior.m
+++ assignment/elle_g/+final/SaturatedLogPosterior.m
@@ -9,4 +9,25 @@ function SatLogPost = SaturatedLogPosterior(parameter, data)
     easy=data.easy;
     med=data.medium;
     hard=data.hard;
+    
+    easyL = -wbllike([Ae, Be], easy);
+    medL = -wbllike([Am, Bm], med);
+    hardL = -wbllike([Ah, Bh], hard);
+    logL= easyL+medL+hardL;
+    
+    % So in the six occurrences of Exp(), 
+    % just replace x by the right A and B values and lambda by 1.  
+    % Then the equation with the six Exp() in it will simplify a lot
+    % lambda * e ^(-lambda * x)
+    
+    lp_Ae=1.*exp(-1.*Ae);
+    lp_Am=1.*exp(-1.*Am);
+    lp_Ah=1.*exp(-1.*Ah);
+    lp_Be=1.*exp(-1.*Be);
+    lp_Bm=1.*exp(-1.*Bm);
+    lp_Bh=1.*exp(-1.*Bh);
+    
+    logP=lp_Ae+lp_Am+lp_Ah+lp_Be+lp_Bm+lp_Bh;
+    
+    SatLogPost=logL+logP;
 end
