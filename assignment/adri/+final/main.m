1a0d92a46e9445447fa419e9154d3c9204f17df8
Adriana <adrifelcha@gmail.com>
2022-03-09 18:23:17 -0800

    Add: Preparing the main.m script for the final submission

--- assignment/adri/+final/main.m
+++ assignment/adri/+final/main.m
@@ -6,25 +6,29 @@ clc
 data = final.getFinalData();
 
 %% Saturated model first
-saturatedTarget = @(parameter) SaturatedLogPosterior(parameter, data); % <-- edit as needed
+saturatedTarget = @(parameter) final.SaturatedLogPosterior(parameter, data);
 saturated = final.Metropolis(saturatedTarget, [2 2 2 2 2 2]');
 saturated.DrawSamples(10000)
 saturated.disp
 
 %% Constrained model next
-constrainedTarget = @(parameter) ConstrainedLogPosterior(parameter, data); % <-- edit as needed
+constrainedTarget = @(parameter) final.ConstrainedLogPosterior(parameter, data);
 constrained = final.Metropolis(constrainedTarget, [2 0 2 0]');
 constrained.DrawSamples(10000)
 constrained.disp
 
 %% Compare the two models
-saturated.DIC - constrained.DIC
+DIC_Comparison = saturated.DIC - constrained.DIC
 
 %% Conclude
-% The model that fits better is the [saturated|constrained] model.
+% The model that fits better is the constrained model.
 %
-% The Scale parameter [goes up|goes down|remains constant] from the "easy"
-% to the "hard" condition. 
+% The Scale parameter goes up from the "easy" to the "hard" condition. 
+[X,~,~] = constrained.CleanHistory;
+Mean_X = mean(X,2);
+scaleEasy = Mean_X(1)
+scaleHard = Mean_X(1) + (2*Mean_X(2))
 %
-% The Shape parameter [goes up|goes down|remains constant] from the "easy"
-% to the "hard" condition. 
+% The Shape parameter goes down from the "easy" to the "hard" condition. 
+shapeEasy = Mean_X(3)
+shapeHard = Mean_X(3) + (2*Mean_X(4))
