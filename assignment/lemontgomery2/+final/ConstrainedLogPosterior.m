d6e02a22a10b1d96f1b4fbb310d3a0cc73f00601
lemontgomery2 <lmontgo1@uci.edu>
2022-03-09 21:30:31 -0800

    specific log posterior function for the constrained model

---
+++ assignment/lemontgomery2/+final/ConstrainedLogPosterior.m
@@ -0,0 +1,13 @@
+function constrainedLogPosterior = ConstrainedLogPosterior(parameter, data)
+
+% Computes the log posterior for the constrained model
+easyParameters = [parameter(1) parameter(3)];
+mediumParameters = [parameter(1)+parameter(2) parameter(3)+parameter(4)];
+hardParameters = [parameter(1)+(2*parameter(2)) parameter(3)+(2*parameter(4))];
+logLikelihood = -wbllike(easyParameters, data.easy) + ...
+                -wbllike(mediumParameters, data.medium) + ...
+                -wbllike(hardParameters, data.hard);
+logPrior = parameter(1) - abs(parameter(2)) + parameter(3) - abs(parameter(4));
+constrainedLogPosterior = logLikelihood * logPrior;
+
+end
