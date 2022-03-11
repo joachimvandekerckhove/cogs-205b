e45d0261e3c65938bbd25b74994d045daae1e2bf
isaacmenchaca <imencha1@uci.edu>
2022-03-10 19:10:37 -0800

    add description of final responses for data.

--- assignment/isaacmenchaca/+final/main.m
+++ assignment/isaacmenchaca/+final/main.m
@@ -23,14 +23,17 @@ constrained.disp
 constrained.plotMetrop("Constrained");
 %% Compare the two models
 disp(saturated.DIC - constrained.DIC);
+% the difference is positive, meaning constrained model is better.
 
 %% Conclude
 % The model that fits better is the [constrained] model.
 %
 % For the Constrained Model, using the mean values from
 % the Metropolis.disp output:
+% (1): The Scale parameter [goes down] from the "easy"
+% to the "hard" condition. 
+% Ae = 2.410, Am = 2.410 - 0.809, Ah = 2.410 - 2(0.809)
 %
-% The Scale parameter [goes down] from the "easy"
+% (2): The Shape parameter [goes down] from the "easy"
 % to the "hard" condition. 
-% The Shape parameter [goes down] from the "easy"
-% to the "hard" condition. 
+% Be = 2.792, Bm = 2.792 - 0.105, Bh = 2.792 - 2(0.105)
