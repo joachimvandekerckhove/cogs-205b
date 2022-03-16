7ffcd360270c554d56f6f33ac58bc62aeea6a8eb
your_username <fatemer@uci.edu>
2022-03-16 14:06:22 -0700

    final assignment

--- assignment/marjan/+final/@Metropolis/disp.m
+++ assignment/marjan/+final/@Metropolis/disp.m
@@ -18,26 +18,3 @@ function disp(obj)
         end
     end
 end
-%     if size(obj.XHistory, 2) == 1
-%         fprintf('Metropolis algorithm not yet ran. Use DrawProposal to do so!\n')
-%     else
-%         [CleanSampleX, ~, ~] = CleanHistory(obj);
-%         fprintf('Estimated Parameters from Metropolis Algorithm:\n')
-%         
-%         meanOfParameters = mean(CleanSampleX, 2);
-%         fprintf('Estimated Mean:\n')
-%         for i = 1:size(CleanSampleX, 1)
-%             fprintf('\t%0.3f', meanOfParameters(i));
-%         end
-%             fprintf('\n')
-%             
-%         stdDevOfParameters = std(CleanSampleX, 0, 2);
-%         fprintf('Estimated Std Dev:\n')
-%         for i = 1:size(CleanSampleX, 1)
-%             fprintf('\t%0.3f', stdDevOfParameters(i));
-%         end
-%             fprintf('\n')
-%         
-%     end
-%     
-% end
