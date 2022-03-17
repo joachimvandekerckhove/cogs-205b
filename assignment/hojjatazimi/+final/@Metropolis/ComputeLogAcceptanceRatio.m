354a98d59a6457fe269f8447304a82bbfcf7ca3e
hojjatazimi <azimi.hojjat@yahoo.com>
2022-03-16 23:29:44 -0700

    init ComputeLogAcceptanceRatio.m

--- assignment/hojjatazimi/+final/@Metropolis/ComputeLogAcceptanceRatio.m
+++ assignment/hojjatazimi/+final/@Metropolis/ComputeLogAcceptanceRatio.m
@@ -1,7 +1,6 @@
 function ComputeLogAcceptanceRatio(obj)
 
 % Computes the logarithm of the acceptance ratio for a Metropolis step
-
-            error('Not yet implemented')
+obj.LogAcceptanceRatio = obj.ProposedPointY - obj.CurrentPointY;
             
 end
