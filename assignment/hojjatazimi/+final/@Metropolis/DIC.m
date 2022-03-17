eded764c0818925951592bbb9a6095a22d58bb2b
hojjatazimi <azimi.hojjat@yahoo.com>
2022-03-17 01:54:43 -0700

    passes all tests!

--- assignment/hojjatazimi/+final/@Metropolis/DIC.m
+++ assignment/hojjatazimi/+final/@Metropolis/DIC.m
@@ -2,8 +2,8 @@ function dic = DIC(obj)
 
 % Computes the approximated DIC of the model
 
-data = final.getFinalData();
-D = -2 * obj.TargetLogPdf(obj.XHistory, data);
+[~, Y] = obj.CleanHistory();
+D = Y;
 dic = mean(D') + var(D')/2;
-            
+    
 end
