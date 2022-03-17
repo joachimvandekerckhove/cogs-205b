ba76f06f73aeddb7d477668bc12e529d170859af
hojjatazimi <azimi.hojjat@yahoo.com>
2022-03-17 00:45:07 -0700

    update disp.m

--- assignment/hojjatazimi/+final/@Metropolis/disp.m
+++ assignment/hojjatazimi/+final/@Metropolis/disp.m
@@ -3,6 +3,14 @@ function disp(obj)
 % This should at a minimum output (a) whether parameters have been estimated and
 % (b) the mean and standard deviation of each parameter if they have been estimated.
 
-    error('Not yet implemented')
+if isempty(obj.XHistory)
+    disp('parameters have not been estimated, yet!')
+else
+    disp('parameters have been estimated!')
+    disp('Mean:')
+    mean(obj.XHistory')
+    disp('Standard Deviation:')
+    std(obj.XHistory')
+end
 
 end
