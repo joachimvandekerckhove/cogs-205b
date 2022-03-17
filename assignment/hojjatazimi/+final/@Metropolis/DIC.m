6909276e2f2b2aef6066c034f4fcaf7518c88475
hojjatazimi <azimi.hojjat@yahoo.com>
2022-03-17 00:58:06 -0700

    init DIC.m

--- assignment/hojjatazimi/+final/@Metropolis/DIC.m
+++ assignment/hojjatazimi/+final/@Metropolis/DIC.m
@@ -1,7 +1,9 @@
-function DIC(obj)
+function dic = DIC(obj)
 
 % Computes the approximated DIC of the model
 
-            error('Not yet implemented')
+data = final.getFinalData();
+D = -2 * obj.TargetLogPdf(obj.XHistory, data);
+dic = mean(D') + var(D')/2;
             
 end
