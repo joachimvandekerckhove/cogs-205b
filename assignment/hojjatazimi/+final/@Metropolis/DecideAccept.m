8dffb2a86cff3b04d338983661e1ba7de2476b09
hojjatazimi <azimi.hojjat@yahoo.com>
2022-03-16 23:32:10 -0700

    init DecideAccept

--- assignment/hojjatazimi/+final/@Metropolis/DecideAccept.m
+++ assignment/hojjatazimi/+final/@Metropolis/DecideAccept.m
@@ -3,6 +3,13 @@ function DecideAccept(obj)
 % Sets the "Accept" property to true if the proposed point should be
 % accepted, and to false otherwise 
 
-            error('Not yet implemented')
+u = unifrnd(0,1);
+log_u = log(u);
+
+if obj.LogAcceptanceRatio > log_u 
+    obj.AcceptProposal = true;
+else
+    obj.AcceptProposal = false;
+end
             
 end
